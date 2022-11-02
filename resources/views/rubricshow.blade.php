7dfc84a947a04122a0bb41ff7003f0dbb1b49848
fferdinandpp <ferditetsuya22@gmail.com>
2022-11-03 00:42:31 +0700

    Create rubricshow.blade.php

---
+++ resources/views/rubricshow.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <title>Document</title>
+  <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <script src="https://kit.fontawesome.com/b79c47ea42.js" crossorigin="anonymous"></script>
+
+  <script src="https://kit.fontawesome.com/61cc44f0a1.js" crossorigin="anonymous"></script>
+
+
+  <style>
+    body {
+      font-family: 'Nunito', sans-serif;
+    }
+  </style>
+
+</head>
+
+<body>
+  <div class="mx-28 pt-14">
+    <nav class="">
+      <ul class="flex space-x-2">
+        <li class="text-[#2E65F3]">Home <span class="mx-2">/</span> </li>
+
+        <li class="text-[#6F727A]">Pemrograman Aplikasi Web <span class="mx-2">/</span> </li>
+      </ul>
+    </nav>
+  </div>
+  <div class="px-28">
+    <div class="flex justify-between border-b-2 border-blue-300 pb-4 ">
+      <div class="judul  ">
+        <h1 class="text-3xl mt-10 font-extrabold" style="font-weight: 900;">Pemrograman Aplikasi Web</h1>
+      </div>
+
+      <div class="">
+        <img src="https://awsimages.detik.net.id/api/wm/2020/06/02/21c64253-0fbd-409c-b32d-adcb304422c6_169.jpeg?wid=54&w=650&v=1&t=jpeg" class="w-14 h-14 rounded-full" alt="">
+      </div>
+
+    </div>
+
+
+
+    
+
+
+
+
+
+
+</body>
+
+</html>
