8b4b97beb465cf88f9f3a91b3b43c0220d5532ae
nabilghi55 <nabilghi55@student.ub.ac.id>
2022-11-02 21:26:21 +0700

    Create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/b79c47ea42.js" crossorigin="anonymous"></script>
+
+  
+
+  <style>
+    body {
+      font-family: 'Inter', sans-serif;
+    }
+   
+  </style>
+</head>
+
+<body>
+  
+</body>
+
+</html>
