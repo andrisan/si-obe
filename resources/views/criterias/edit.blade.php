06040c46259875694b99b5688967a563bf26312f
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2022-11-02 18:38:18 +0700

    new file

---
+++ resources/views/criterias/edit.blade.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+         <div class="flex h-screen justify-center items-center bg-indigo-100">
+            <div class="m-auto bg-white p-8 rounded-2xl">
+                <h1 class="mb-4 text-4xl font-bold tracking-tight leading-none text-indigo-400 md:text-5xl lg:text-6xl dark:text-white pb-14">
+                Criterias<span class="bg-blue-100 text-blue-800 text-3xl font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-2">EDIT</span>
+                </h1>
+                <form action="foo/barr">
+                    <div class="pb-4">
+                        <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Title</strong></label></div>
+                        <input type="text" placeholder="Posisi CLO" class="input input-bordered w-full max-w-xs" /> <br>
+                    </div>
+                    <div class="pb-4">
+                    <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Description</strong></label></div>
+                        <textarea class="textarea textarea-bordered w-full" placeholder="Deskripsi CLO"></textarea>
+                    </div>
+                    <div class="pb-4">
+                        <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Max Point</strong></label></div>
+                        <input type="text" placeholder="Posisi CLO" class="input input-bordered w-full max-w-xs" /> <br>
+                    </div>
+                </form>
+                <input type="submit" value="Edit" class="btn btn-active rounded-md " />
+                <button class="btn btn-outline rounded-md">Cancel</button>
+            </div>
+         </div> 
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/rubrics/{rubric}/criterias/{criteria}/edit', function () {
+    return view('criterias.edit');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
