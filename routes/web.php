7a7f2ab0407420ff57c828bd65dc522886abc4c3
fferdinandpp <ferditetsuya22@gmail.com>
2022-11-03 00:42:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/rubric/show', function () {
+    return view('rubricshow');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
