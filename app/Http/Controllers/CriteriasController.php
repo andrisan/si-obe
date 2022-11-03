785e50000595c45fa4f9c5d20c6b855b4f807487
bariqalfariz <bariqalfariz09@gmail.com>
2022-11-03 13:15:02 +0700

    Update CriteriasController.php

--- app/Http/Controllers/CriteriasController.php
+++ app/Http/Controllers/CriteriasController.php
@@ -23,7 +23,7 @@ class CriteriasController extends Controller
      */
     public function create()
     {
-        
+        return view('criterias.create');
     }
 
     /**
@@ -45,7 +45,7 @@ class CriteriasController extends Controller
      */
     public function show($id)
     {
-        
+        return 'criterias'. $id;
     }
 
     /**
