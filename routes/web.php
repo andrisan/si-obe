398b54aae290ac8a0389ffa3572e989de9d5a3b0
marsyanadia <deswitamn@student.ub.ac.id>
2022-11-03 14:59:32 +0700

    route syllabi create

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/syllabi_new', function () {
-    return view('syllabi_new');
+Route::get('/syllabi-new', function () {
+    return view('syllabi-new');
 });
 
 require __DIR__.'/auth.php';
