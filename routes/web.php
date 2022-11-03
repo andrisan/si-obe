12231886016dedd76113ff56887c8f17c0161448
MuhAtthar <muhatthar@gmail.com>
2022-11-03 13:20:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\iloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('ilo', iloController::class)->middleware(['auth'])->only('show', 'create');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
