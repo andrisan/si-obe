06849d628f57e05b9f58bd7bad0c2697a5c5eb31
Gazee <100186961+Gazee1112@users.noreply.github.com>
2022-11-03 15:50:06 +0700

    edit department
    mengedit deparment edit sesuai dengan apps layout

--- resources/views/departments/edit.blade.php
+++ resources/views/departments/edit.blade.php
@@ -1,41 +1,38 @@
-<!DOCTYPE html>
-<html data-themes="dark" lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
 
-<head>
-  <link href="https://cdn.jsdelivr.net/npm/daisyui@2.38.0/dist/full.css" rel="stylesheet" type="text/css" />
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>Edit Departemen</title>
-</head>
-
-<body>
-  <div class="hero min-h-screen bg-base-100">
-    <div class="hero-content text-center">
-      <div class="max-w-xl">
-        <div class="card w-96 bg-base-300 text-neutral-content">
-          <div class="card-body items-center text-center">
-            <h2 class="card-title">Edit Departemen</h2>
-            <div class="form-control w-full max-w-xs">
-              <label class="label">
-                <span class="label-text">Fakultas</span>
-              </label>
-              <select class="select select-bordered w-full max-w-xs">
-                <option>Ilmu Komputer</option>
-                <option>Ekonomi Bisnis</option>
-              </select>
-              <label class="label mt-4">
-                <span class="label-text">Departemen</span>
-              </label>
-              <input type="text" class="input input-bordered w-full max-w-xs" value="Sistem Informasi" />
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg ">
+                <div class="p-6 bg-white border-b border-gray-200 flex justify-center">
+                  <div class="card w-96 bg-base-300 text-neutral-content">
+                    <div class="card-body items-center text-center">
+                      <h2 class="card-title text-neutral">Edit Departemen</h2>
+                      <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                          <span class="label-text">Fakultas</span>
+                        </label>
+                        <select class="select select-bordered w-full max-w-xs text-neutral">
+                          <option>Ilmu Komputer</option>
+                          <option>Ekonomi Bisnis</option>
+                        </select>
+                        <label class="label mt-4 text-neutral">
+                          <span class="label-text">Departemen</span>
+                        </label>
+                        <input type="text" class="input input-bordered w-full max-w-xs text-neutral" value="Sistem Informasi" />
+                      </div>
+                      <div class="card-actions justify-end mt-4">
+                        <button class="btn btn-outline btn-error">Cancel</button>
+                        <button class="btn btn-primary">Simpan</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             </div>
-            <div class="card-actions justify-end mt-4">
-              <button class="btn btn-outline btn-error">Cancel</button>
-              <button class="btn btn-primary">Simpan</button>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-</body>
-
-</html>
+</x-app-layout>
