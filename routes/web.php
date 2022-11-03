189d5f73491d65b1c4f0247e47a100473e9e446d
nabilghi55 <nabilghi55@student.ub.ac.id>
2022-11-03 23:10:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/rubric/edit', function () {
-    return view('rubricedit');
+    return view('rubrics.edit');
 });
 
 Route::get('/dashboard', function () {
