3678772f9c8e62265f1594d56d08c14e3a7b7047
zaydansss <zaydans@student.ub.ac.id>
2022-11-03 15:47:52 +0700

    Update learning-plans.index.blade.php

--- resources/views/learning-plans.index.blade.php
+++ resources/views/learning-plans.index.blade.php
@@ -19,7 +19,7 @@
     </div>
     <div class="col-span-1 grid grid-flow-row grid-rows-none auto-rows-max divide-y overflow-y-scroll h-full">
       <div class="text-xl font-bold p-5">
-        <label>My Course</label>
+        <label>Learning Plans</label>
       </div>
       <div class="grid grid-flow-row grid-rows-none p-5">
         <div class="flex flex-wrap py-2">
@@ -28,7 +28,7 @@
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
           </svg>
-          <label class="px-4">Course 1</label>
+          <label class="px-4">Learning Plans 1</label>
         </div>
         <div class="flex flex-wrap py-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -36,7 +36,7 @@
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
           </svg>
-          <label class="px-4">Course 2</label>
+          <label class="px-4">Learning Plans 2</label>
         </div>
         <div class="flex flex-wrap py-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -44,7 +44,7 @@
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
           </svg>
-          <label class="px-4">Course 3</label>
+          <label class="px-4">Learning Plans 3</label>
         </div>
         <div class="flex flex-wrap py-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -52,7 +52,7 @@
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
           </svg>
-          <label class="px-4">Course 4</label>
+          <label class="px-4">Learning Plans 4</label>
         </div>
         <div class="flex flex-wrap py-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -60,7 +60,7 @@
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
           </svg>
-          <label class="px-4">Course 5</label>
+          <label class="px-4">Learning Plans 5</label>
         </div>
       </div>
       <div class="grid justify-end row-end-auto p-5 ">
@@ -72,9 +72,7 @@
     </div>
     <div class="col-span-3 p-10 shadow-xl justify-items-auto overflow-y-auto">
       <h1 class="text-4xl font-semibold text-primary-content">Learning Plans</h1>
-      <div class="grid grid-cols-8 gap-4 py-4">
-        <button class="btn btn-accent col-end-9">Tambah</button>
-      </div>
+      
       <div class="overflow-x-auto py-4">
         <table class="table table w-full">
           <!-- head -->
@@ -178,6 +176,9 @@
               </tr>
           </tbody>
         </table>
+      </div>
+      <div class="grid grid-cols-8 gap-4 py-4">
+        <button class="btn btn-accent col-end-9">Tambah</button>
       </div>
       <form>
           <div class="flex justify-end p-4">
