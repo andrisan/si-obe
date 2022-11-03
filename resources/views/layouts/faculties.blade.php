d4ceb395732c935290720d366401ec1db60b0d22
Ahmad Fathoni <99490295+Fathoni243@users.noreply.github.com>
2022-11-03 12:53:22 +0700

    Edit Faculties
    edit faculties.index, add layouts.faculties, add componentsFaculties

---
+++ resources/views/layouts/faculties.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.38.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Fakultas</title>
+</head>
+<body>
+    <div class="container p-12">
+        {{-- Page Content --}}
+        {{ $slot }}
+    </div>
+</body>
+</html>
