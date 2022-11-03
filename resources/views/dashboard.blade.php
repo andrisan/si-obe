1f73bdf57c94a8f9272fb154846b0beb4e576829
Eci Emriyeni <112626798+eciemriyeni@users.noreply.github.com>
2022-11-03 22:38:14 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,37 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+        <div class="flex justify-between">
+            <div class="items-start">
+                <h2 class="font-semibold   text-4xl text-gray-800 leading-tight">
+                    {{ __('LK-01') }}
+                </h2>
+            </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+            <div class="items-end">
+                <img src="{{ asset('img/Vector(2).png') }}" alt="">
             </div>
         </div>
-    </div>
+
+    </x-slot>
+
+    <div class="py-12 ">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white border-2 border-blue-500 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex justify-between">
+                        <h1 class="text-[#2E65F3] font-extrabold items-start">N01-1</h1>
+                        <div class="items-end">
+                            <h1 class="font-extrabold text-black">/3</h1>
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                   
+                   
+                
+              </div>
+          </div>
+      </div>
+  </div>
 </x-app-layout>
