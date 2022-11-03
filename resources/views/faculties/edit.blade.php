c934a31cc9aaf87eea914a0d944ec2e689497df2
RazzaaqZul <razzaaqzulkahfi123@gmail.com>
2022-11-03 11:50:28 +0700

    Edit Fakultas

---
+++ resources/views/faculties/edit.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html data-themes="dark" lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.38.0/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Edit Fakultas</title>
+</head>
+<body>
+    <div class="hero min-h-screen bg-base-100">
+        <div class="hero-content text-center">
+          <div class="max-w-xl">
+            <div class="card w-96 bg-base-300 text-neutral-content">
+                <div class="card-body items-center text-center">
+                  <h2 class="card-title">Edit Fakultas</h2>
+                  <div class="form-control w-full max-w-xs">
+                    <label class="label">
+                      <span class="label-text">Nama Fakultas</span>
+                    </label>
+                    <input type="text" class="input input-bordered w-full max-w-xs" value="Ilmu Komputer"/>
+                  </div>
+                  <div class="card-actions justify-end mt-4">
+                      <button class="btn btn-outline btn-error">Cancel</button>
+                      <button class="btn btn-primary">Simpan</button>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/faculties/edit', function () {
+    return view('faculties.edit');
+});
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
