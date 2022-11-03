ad395291b5fdc211c0d8b158b0126e3fd3d98afa
putriraiss <putridaliana@gmail.com>
2022-11-03 13:07:36 +0700

    syllabi.index

---
+++ resources/views/syllabi-index.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="dark">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="https://cdn.jsdelivr.net/npm/daisyui@2.38.0/dist/full.css" rel="stylesheet" type="text/css" />
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  <title>Document</title>
+
+</head>
+
+<body>
+  <!--Nvabr-->
+  <div class="navbar bg-base-100">
+    <div class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+        </svg>
+      </button>
+    </div>
+    <div class="flex-1">
+      <a class="btn btn-ghost normal-case text-xl">RPS</a>
+    </div>
+    <div class="flex-none gap-4">
+      <a class="pr-3">
+        <p class="text-m">Putri Daliana Salsabilla Rais</p>
+        <p class="text-x5">215150700111043</p>
+      </a>
+    </div>
+    <div class="dropdown dropdown-end">
+      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <img src="https://placeimg.com/80/80/people" />
+        </div>
+      </label>
+    </div>
+  </div>
+  <div class="border-b border-slate-500"></div>
+  <!--EndNvbr-->
+
+  <!--Isi-->
+  <div class="container pt-10">
+    <div class="grid grid-cols-2 gap-4 justify-items-center">
+
+    <div class="card bg-base-100 shadow-xl" style="border-width:2px; height:250px; width:500px">
+        <div class="card-body">
+          <h2 class="card-title">TITLE</h2>
+          <p>Penjelasan mengenai TITLE</p>
+          <div class="border-b"></div>
+          <div class="card-actions justify-end">
+            <p align="right">Author</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card bg-base-100 shadow-xl" style="border-width:2px; height:250px; width:500px">
+        <div class="card-body">
+          <h2 class="card-title">TITLE</h2>
+          <p>Penjelasan mengenai TITLE</p>
+          <div class="border-b"></div>
+          <div class="card-actions justify-end">
+            <p align="right">Author</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card bg-base-100 shadow-xl" style="border-width:2px; height:250px; width:500px">
+        <div class="card-body">
+          <h2 class="card-title">TITLE</h2>
+          <p>Penjelasan mengenai TITLE</p>
+          <div class="border-b"></div>
+          <div class="card-actions justify-end">
+            <p align="right">Author</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="card bg-base-100 shadow-xl" style="border-width:2px; height:250px; width:500px">
+        <div class="card-body">
+          <h2 class="card-title">TITLE</h2>
+          <p>Penjelasan mengenai TITLE</p>
+          <div class="border-b"></div>
+          <div class="card-actions justify-end">
+            <p align="right">Author</p>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+
+
+  <!--EndIsi-->
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/syllabi-index', function () {
+    return view('syllabi-index');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
