41d34fe00b885d6ad91a10147d0ff9d5da772419
ubedillahtaufik <90669031+ubedillahtaufik@users.noreply.github.com>
2022-11-03 18:31:22 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-8xl sm:px-6 lg:px-8">
             <div class="bg-white overflow- shadow-sm sm:rounded-lg">
                 <div class="p-1 bg-white border-b border-gray-200">
-
+                     <div class="md:ml-20">
+                        <div class="rounded-md p-2 w-72 bg-[#AFC7F5]  ">
+                            <div class="flex text-center items-center  ">
+                                <div class="gb ">
+                                    <img src="{{ asset('img/Gambar Fakultas.png') }}" alt="">
+                                </div>
+                                <h2 class="ml-3 text-xl text-black">All Faculties</h2>
+                            </div>
+                        </div>
+                    </div>
                     
                 </div>
             </div>
