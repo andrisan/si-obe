2e11253bcc45c662bd0b26eb8ab63d0cbf7f5dfb
zaydansss <zaydans@student.ub.ac.id>
2022-11-03 15:30:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/syllabi/{syllabus}/learning-plans', function () {
+    return view('learning-plans.index.blade.php');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
