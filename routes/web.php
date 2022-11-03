35715beec254907f919c47d9335cb23a6376edba
Rizka Winal Sabrina <103319544+rizkawinalsabrina@users.noreply.github.com>
2022-11-03 14:13:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\rubricController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('rubrics',rubricController::class);
 Route::get('/rubric/show', function () {
     return view('rubricshow');
 })->middleware(['auth']);
