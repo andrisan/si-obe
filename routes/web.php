b76b46505954e03a18d1cc3fbb7b2f0579ab9467
fairuznp <74838107+Fairuz5@users.noreply.github.com>
2022-11-03 14:52:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/course-create', function () {
+    return view('course-create');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
