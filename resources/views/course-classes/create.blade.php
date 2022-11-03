bc6ef8092aa6ca3087d0e6ec8d08e9f752b40dbd
alhavydz <jundullah0853@student.ub.ac.id>
2022-11-03 15:21:17 +0700

    penghapusan search button
    pengurangan margin

--- resources/views/course-classes/create.blade.php
+++ resources/views/course-classes/create.blade.php
@@ -13,27 +13,18 @@
                         <div class="container p-4 text-primary mt-2 text-sm">
                             <form>
                                 <label class="uppercase font-bold  text-blue-600" for="fname">nama kelas</label><br>
-                                <input type="text" placeholder="Enter Your Username" class="input input-bordered input-ghost input-l w-full max-w-xl mb-4 mt-2" />
+                                <input type="text" placeholder="Enter Your Username" class="input input-bordered input-ghost input-l w-full max-w-xl mb-2 mt-2" />
+                            </form>
+                        </div>
+
+                        <div class="container p-4 text-primary text-sm">
+                            <form>
+                                <label class="uppercase font-bold  text-blue-600" for="fname">kode kelas</label><br>
+                                <input type="text" placeholder="Enter Your Class Code" class="input input-bordered input-ghost input-l w-full max-w-xl mb-4 mt-2" />
                             </form>
                         </div>
             
-                        <div class="form-control ml-4 text-sm max-w-xl mb-0" id="kodekelas">
-                            <label class="uppercase font-bold  text-blue-600" for="fname">kode kelas</label>
-                            <div class="input-group mt-2">
-                                <input type="text" placeholder="Enter Your Class Code" class="input w-full input-ghost" />
-                                <select class="select select-ghost">
-                                    <option>CCPAW001</option>
-                                    <option>CCPAW002</option>
-                                    <option>CCPAW003</option>
-                                    <option>CCPAW004</option>
-                                </select>
-                                <button class="btn btn-square bg-gray-200 border-none">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                                </button>
-                            </div>
-                        </div>
-            
-                        <div class="max-w-xl mt-9 ml-4">
+                        <div class="max-w-xl ml-4">
                             <label class="uppercase font-bold text-sm text-blue-600" for="fname">unggah</label>
                             <label
                                 class="flex mt-2 justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
@@ -56,7 +47,7 @@
                         </div>
                         <br>
                         <br>
-                        <div class="mt-10 ml-4">
+                        <div class="mt-4 ml-4">
                             <button class="bg-blue-600 hover:bg-white hover:text-blue-600 text-white font-bold py-2 px-4 rounded border-blue-600 border-2">
                                 Create Class
                             </button>
