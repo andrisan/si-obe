bba4983510c8e6975a0d9104b7b21ecda903e49c
nabilghi55 <nabilghi55@student.ub.ac.id>
2022-11-03 11:35:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dd', function () {
+    return view('dashboard');
+});
 
 Route::get('/home', function () {
     return view('home');
