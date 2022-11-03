ed92ab2e5b578c84411480dd5a06f66348808d73
MuhammadIrtada <muhammadirtada@gmail.com>
2022-11-03 14:09:19 +0700

    perubahan course-classes/edit

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/course-classes/{test}/edit', function () {
+    return view('course-classes/edit');
+})->middleware(['auth']);
+
 require __DIR__.'/auth.php';
