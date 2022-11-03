32460241170816a5645c0319ac6d744dcdbbdcfc
fingkaaprilia22 <103511988+fingkaaprilia22@users.noreply.github.com>
2022-11-03 22:46:47 +0700

    Update criteriaController.php

--- app/Http/Controllers/criteriaController.php
+++ app/Http/Controllers/criteriaController.php
@@ -47,3 +47,38 @@ class criteriaController extends Controller
     {
         return view('criterias.show');
     }
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
