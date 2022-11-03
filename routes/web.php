82c881727bccc666332d2e30463b4d482bf0ff64
A. Fauzi <100508741+Aither-24@users.noreply.github.com>
2022-11-03 15:18:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/courses/{course}/edit', function () {
+    return view('coursesedit');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
