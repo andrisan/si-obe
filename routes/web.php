8b9a2dd40463d19ea42881df338fd2b91a30a2c7
MuhammadIrtada <muhammadirtada@gmail.com>
2022-11-03 13:43:11 +0700

    update create 42

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/course-classes/tes', function () {
+    return view('course-classes/tes');
+})->middleware(['auth'])->name('course-classes/tes');
+
 require __DIR__.'/auth.php';
