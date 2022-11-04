60ac7e2ab5f3e6f8918af989e3c5932999644668
Eci Emriyeni <112626798+eciemriyeni@users.noreply.github.com>
2022-11-04 15:10:35 +0700

    Update create.blade.php

--- resources/views/criteria/create.blade.php
+++ resources/views/criteria/create.blade.php
@@ -9,16 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class=" bg-white border-b border-gray-200">
-                    <div class="mx-">
-                        <div class=" ">
-                            <ul>
-                               
-                              
-                            </ul>
+                    <div class = "p-10">
+                <h1 class="text-black">I N D I C A T O R</h1>
+                        <div class="mt-2">
+                            <input class="rounded-md w-80" placeholder="Enter text here..." type="text">
                         </div>
-                        <div>
-                
+                        
+                        
+                        
+                        
+                        
+                        
                         </div>
+                         </div>
                     </div>
                    
                 </div>
