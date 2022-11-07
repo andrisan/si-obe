eb308c10a62ce59c66036205f013b0e391d2777c
marsyanadia <deswitamn@student.ub.ac.id>
2022-11-07 21:43:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/courses.create', function () {
+Route::get('/courses/create', function () {
     return view('courses.create');
 })->middleware(['auth']);
 
