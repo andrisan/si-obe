caf4b073508a5f163552a544b11356f485279e2c
Moch Danial Rasyid Akbar S <danialrasyid1@student.ub.ac.id>
2022-11-08 16:50:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/course-classes/index', [CourseClassesController::class,'index'])->middleware(['auth']);
