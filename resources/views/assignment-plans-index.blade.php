ee05c4d0b4a512d6677a18b4e07b0b57c784fb28
claireYng <claraclaritayng@gmail.com>
2022-11-08 20:45:30 +0700

    Update assignment-plans-index.blade.php

--- resources/views/assignment-plans-index.blade.php
+++ resources/views/assignment-plans-index.blade.php
@@ -1,102 +1,110 @@
 <!DOCTYPE html>
-<html lang="en" data-theme="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html lang="en" data-theme="light">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.38.0/dist/full.css" rel="stylesheet" type="text/css" />
-  <script src="https://cdn.tailwindcss.com"></script>
+      <link href="https://cdn.jsdelivr.net/npm/daisyui@2.38.0/dist/full.css" rel="stylesheet" type="text/css" />
+      <script src="https://cdn.tailwindcss.com"></script>
 
-    <title>Document</title>
-</head>
-<body style="background-color: #292929">
+      <title>Assignments Plans</title>
+  </head>
+  <body>
+    <!-- navbar--> 
+    <div class="navbar">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost btn-circle mx-10 mt-5">
+            <button class="btn btn-circle btn-outline btn-info">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+            </button>
+          </label>
+        </div>
+      </div>
 
-<!-- navbar--> 
-<div class="navbar" style="background-color: #292929">
-  <div class="navbar-start">
-    <div class="dropdown">
-      <label tabindex="0" class="btn btn-ghost btn-circle">
-        <img src="img/eks.png" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"/>
+      <div class="navbar-center" >
+        <a class="normal-case text-xl">Assignment Plans</a>
+      </div>
+    
+    </div>
+    <!--divider-->
+      <div class="flex flex-col w-full">
+        <div class="divider"></div> 
+      </div>
+    <!--end divider-->
+    </div>
+<!--end navbar-->
+
+    <!---Assignment style-->
+    <select class="select select-bordered border-info w-full max-w-xs mx-10 mb-10 mt-5">
+      <option disabled selected>Assignment Style</option>
+      <option>Multiple choice</option>
+      <option>Essay</option>
+      <option>Simple question</option>
+    </select>
+    <!--end Assignment style-->
+
+    <!--deadline -->
+    <div class="form-control float-right mx-10 mt-5">
+      <label class="input-group input-group-md" >
+        <span><img src="img/Vector.png"/></span>
+        <input type="text" placeholder="DD/MM/YYYY" class="input input-bordered input-md mb-0"/>
       </label>
     </div>
-  </div>
+    <!--end deadline-->
 
-  <div class="navbar-center" >
-    <a class="normal-case text-xl" style="color: #BB86FC; font-size: 30px;">Assignment Plans</a>
-  </div>
-  <div class="navbar-end">
-    <button class="btn" style="background-color: #BB86FC; text-shadow: 0px 0px 4px white; color: white;">Simpan</button>
-  </div>
-</div>
-  <div class="flex flex-col w-full">
-    <div class="divider"></div> 
-  </div>
-</div>
-
-<main style="padding: 25px; overflow: auto; color: white;">
-<aside style= "float: right; width: 25%; padding-left: 20px;">
-
-<!--deadline -->
-<div class="form-control">
-  <label class="input-group input-group-md" >
-    <span><img src="img/Vector.png"/></span>
-    <input type="text" placeholder="DD/MM/YYYY" class="input input-bordered input-md mb-0" style="border-color: #BB86FC; background-color: #292929" />
-  </label>
-</div>
-
-<!-- instruction-->
-<div class="form-control mt-9">
-  <label class="label">
-    <span class="label-text">Submission Instruction</span>
-  </label> 
-  <textarea class="textarea textarea-bordered h-24 mb-5" placeholder="Text here" style="background-color: #121212; height: 250px;"></textarea>
-</div>
-
-<!---Assignment style-->
-<select class="select w-full max-w-xs mb-5 mt-5" style="border-color: #BB86FC; background-color: #292929">
-  <option disabled selected>Assignment style</option>
-  <option>Multiple choice</option>
-  <option>Essay</option>
-  <option>Simple question</option>
-</select>
-
-</aside>
-<!-- card title-->
-  <div>
-    <div class="card w-96 shadow-xl mb-10" style="float: left; width:75%; background-color: #35303A;" >
-      <div class="card-body" style="padding: 5px 40px;border-radius: 5px; margin-top: 20px;">
-        <div class="form-control w-full">
-          <input type="text" placeholder="Title" class="input input-bordered w-full mb-5" style="background-color: #121212;"/>
-          <div class="form-control mb-5">
-            <textarea class="textarea textarea-bordered h-24" placeholder="Description" style="background-color: #121212; height: 250px;"></textarea>
+    <!-- card title-->
+    <div class="card float-left shadow-xl mx-10">
+        <div class="card w-96 shadow-xl mt-10 mx-10  bg-stone-300">
+          <div class="card-body card-bordered">
+            <div class="form-control w-full">
+              <input type="text" placeholder="Title" class="input input-bordered w-full mb-5"/>
+              <div class="form-control mb-5">
+                <textarea class="textarea textarea-bordered h-24" placeholder="Description"></textarea>
+              </div>
+              <div class="form-control w-52">
+                <label class="cursor-pointer label">
+                  <span class="label-text">Group Assignment</span> 
+                  <input type="checkbox" class="toggle toggle-info" checked />
+                </label>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- card output-->
-  <!--<div class="card w-96 bg-base-100 shadow-xl mt-5">
-  <div class="card-body" style="padding: 5px 40px;border-radius: 5px; margin-top: 20px;">
-    <h2 class="card-title">Output</h2>
-    <textarea class="textarea textarea-bordered" placeholder="Text"></textarea>
-  </div>
-</div> -->
-<div>
-    <div class="card w-96 bg-base-100 shadow-xl mb-10" style="float: left; width:75%; background-color: #35303A;">
-      <div class="card-body" style="padding: 5px 40px;border-radius: 5px; margin-top: 20px;">
-      <h2 class="card-title">Output</h2>
-        <div class="form-control w-full">
-          <div class="form-control mb-5">
-            <textarea class="textarea textarea-bordered h-24" placeholder="Text here" style="background-color: #121212;"></textarea>
+        <!--output-->
+        <div>
+          <div class="card w-96 bg-base-100 shadow-xl mt-10 mb-10 mx-10">
+            <div class="card-body bg-stone-300">
+            <h2 class="card-title">Output</h2>
+              <div class="form-control w-full">
+                <div class="form-control mb-5">
+                  <textarea class="textarea textarea-bordered h-24" placeholder="Text here"></textarea>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      <!--end output-->
     </div>
-  </div>
+    <!--end card -->
 
-</div>
-</main>
-</body>
+    <!--  submissioninstruction-->
+    <div class="form-control mx-10">
+          <label class="label">
+            <span class="label-text">Submission Instruction</span>
+          </label> 
+          <textarea class="textarea textarea-bordered mt-5 mb-5 h-24" placeholder="Text here"></textarea>
+        </div>
+<!--button-->
+<div class="navbar-end mx-10 mt-5">
+        <button class="btn btn-info">Simpan</button>
+      </div>
+    <!--end button-->
+        <!--end instruction-->
+
+      
+
+    </div>
+  </body>
 </html>
