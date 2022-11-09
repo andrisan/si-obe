5478aeafd2fcae8164a5eee08b0d64b0e7e15aac
Ardhi Wahyu Hidayat <ardhiwahyu@student.ub.ac.id>
2022-11-09 18:23:45 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/departments/{department}/study-programs', [StudyProgramController::class, 'index']);
+
 require __DIR__.'/auth.php';
