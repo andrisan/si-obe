46f37718cbe1d057d1a7ba59ae4fa690e9dcadad
asyamadil2 <asyamadil@gmail.com>
2022-11-09 18:50:42 +0700

    Update show_join.blade.php

---
+++ resources/views/course-classes/show_join.blade.php
@@ -0,0 +1,24 @@
+<script src="https://kit.fontawesome.com/01f732cd61.js" crossorigin="anonymous"></script>
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Course Classes') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-20">
+                    <div class="flex justify-between border-b-2 pb-4 ">
+                        <div class="judul text-black  ">
+                            <h1 class="text-2xl font-extrabold" style="font-weight: 900;">Course Classes</h1>
+                        </div>
+                    </div>
+            
+                    <nav class="">
+                        <ul class="flex space-x-2 font-extrabold">
+                            <li class="text-[#2E65F3]">Course Classes <span class="mx-2">/</span> </li>
+                            <li class="text-black"> Join <span class="mx-2"></span> </li>
+                        </ul>
+                    </nav>
