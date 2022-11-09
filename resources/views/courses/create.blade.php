a74d9db7e65366a6c89b0a831adbd87a2d37d02f
fairuznp <74838107+Fairuz5@users.noreply.github.com>
2022-11-09 14:37:57 +0700

    Update create.blade.php

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -18,9 +18,9 @@
     <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content flex flex-col px-10 pt-5 border-primary-content border-l border-slate-500">
       <!-- Page content here -->
-      <div class="">
-        <div class="border-transparent rounded-lg bg-white px-5 py-5">
-          <div class="grid grid-cols-2 grid-flow-row gap-1 justify-items-center">
+      <div class="pb-10">
+        <div class="border-b rounded-lg shadow-xl bg-white px-5 py-5">
+          <div class="grid grid-cols-2 grid-flow-row gap-1 justify-items-center pb-5">
 
             <div class="form-control w-full max-w-xs">
               <label class="label">
@@ -83,22 +83,24 @@
                 <span class="label-text text-neutral font-bold">Course Type</span>
               </label>
               <select class="select text-neutral input-bordered bg-white w-full max-w-xs">
+                <option></option>
                 <option>Mandatory</option>
                 <option>Elective</option>
               </select>
             </div>
-            <!-- <div class="form-control w-full max-w-xs">
-              <label class="label">
-                <span class="label-text text-blue-600 text-blue-600">Course Type</span>
-              </label>
-              <input type="text" class="input text-blue-600 input-bordered input-primary bg-white w-full max-w-xs" />
-            </div> -->
 
             <div class="form-control w-full max-w-xs">
               <label class="label">
                 <span class="label-text text-neutral font-bold">Study Program</span>
               </label>
-              <input type="text" class="input text-neutral input-bordered bg-white w-full max-w-xs" />
+              <select class="select text-neutral input-bordered bg-white w-full max-w-xs">
+                <option></option>
+                <option>Sistem Informasi</option>
+                <option>Teknologi Informasi</option>
+                <option>Pendidikan Teknologi Informasi</option>
+                <option>Teknik Informatika</option>
+                <option>Teknik Komputer</option>
+              </select>
             </div>
 
             <div class="form-control w-full max-w-xs">
@@ -108,13 +110,17 @@
               <input type="text" class="input text-neutral input-bordered bg-white w-full max-w-xs" />
             </div>
 
+            <div class="pt-4" style="float:right">
+              <button class="mt-12 btn btn-outline">Create</button>
+            </div>
+            <!-- <div class="px-10 pt-3 border-gray-400" style="float:right">
+            <a class="btn btn-outline " href="/syllabi">Save</a>
+          </div> -->
           </div>
         </div>
 
 
-        <div class="py-5" style="float:right">
-          <button class="btn btn-outline justify-end">Create</button>
-        </div>
+
       </div>
       <!-- End content -->
     </div>
