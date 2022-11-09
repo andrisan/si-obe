d63fe550933971d266ccce603fa66ec2ed254acb
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2022-11-09 15:00:53 +0700

    Create StudentGradeController.php

---
+++ app/Http/Controllers/StudentGradeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentGradeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($id)
+    {
+        return view('layouts.studentgrades');
+    }
