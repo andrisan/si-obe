1c2580cea75da772003417e4da230a412f38bfab
auliasanggraini <anggirhama12@gmail.com>
2022-11-09 09:31:53 +0700

    Update create.blade.php

--- resources/views/syllabi/create.blade.php
+++ resources/views/syllabi/create.blade.php
@@ -1,36 +1,51 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Create Syllabus') }}
+      {{ __('Syllabi Create') }}
     </h2>
   </x-slot>
 
-  <div class="mx-10 my-5">
-    <div class="border-b rounded-lg bg-base-300 px-4 py-5">
-      <div class="form-control w-full max-w-xs">
-        <label class="label">
-          <span class="label-text text-blue-600 text-blue-600">Syllabus Name</span>
-        </label>
-        <input type="text" class="input text-blue-600 input-bordered input-primary bg-white w-full max-w-xs" />
+  <div class="text-sm breadcrumbs pl-8 pt-5 font-bold text-primary">
+    <ul>
+      <li><a href="">Dashboard</a></li>
+      <li><a href="">Syllabi</a></li>
+      <li>Syllabi Create</li>
+    </ul>
+  </div>
+
+  <div class="px-8 pt-3 pb-10">
+    <div class="border-b rounded-lg bg-primary-content shadow-xl px-5 py-5">
+
+      <div class="grid grid-cols-2 pb-5">
+        <div>
+          <div class="form-control w-full px-10">
+            <label class="label">
+              <span class="label-text text-neutral font-bold">Syllabus Name</span>
+            </label>
+            <input type="text" class="input text-neutral input-bordered input-primary bg-white w-full h-18" />
+          </div>
+          <div class="form-control w-full px-10">
+            <label class="label">
+              <span class="label-text text-neutral font-bold">Author</span>
+            </label>
+            <input type="text" class=" input text-neutral input-bordered input-primary bg-white w-full h-18" />
+          </div>
+        </div>
+
+        <div>
+          <div class="form-control w-full px-10">
+            <label class="label">
+              <span class="label-text text-neutral font-bold">Head of Study Program</span>
+            </label>
+            <textarea class="textarea text-neutral input-bordered input-primary bg-white w-full h-full"></textarea>
+          </div>
+
+          <div class="px-10 pt-3" style="float:right">
+            <button class="btn btn-outline btn-primary" href="/syllabi">Create</button>
+          </div>
+        </div>
       </div>
-    
-      <div class="form-control w-full max-w-xs">
-        <label class="label">
-          <span class="label-text text-blue-600 text-blue-600">Author</span>
-        </label>
-        <input type="text" class=" input text-blue-600 input-bordered input-primary bg-white w-full max-w-xs" />
-      </div>
-    
-      <div class="form-control">
-        <label class="label">
-          <span class="label-text text-blue-600 text-blue-600">Head of Study Program</span>
-        </label> 
-        <textarea class="textarea text-blue-600 input-bordered input-primary bg-white w-full h-24"></textarea>
-      </div>
-    </div>
-  
-    <div class="py-5" style="float:right">
-      <a class="btn btn-outline justify-end" href="/syllabi">Create</a>
+
     </div>
   </div>
 </x-app-layout>
