7f186829708f386fb6ff9b87ad5fba7f4d825bdc
surjajanh <alfiyantogw@student.ub.ac.id>
2022-11-09 15:05:53 +0700

    Update StudentGradeController.php

--- app/Http/Controllers/StudentGradeController.php
+++ app/Http/Controllers/StudentGradeController.php
@@ -57,3 +57,27 @@ class StudentGradeController extends Controller
     {
         //
     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
