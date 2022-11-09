b56ec019efd65f09a90a78b86561df053092778d
A. Fauzi <100508741+Aither-24@users.noreply.github.com>
2022-11-09 22:35:36 +0700

    Update create.blade.php

--- resources/views/learning-plans/create.blade.php
+++ resources/views/learning-plans/create.blade.php
@@ -1,23 +1,23 @@
-<!DOCTYPE html>
-<html data-theme="dark" lang="en">
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <div class="items-start">
+                <h2 class="font-semibold   text-4xl text-gray-800 leading-tight">
+                    {{ __('Learning Plans Create') }}
+                </h2>
+            </div>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@2.38.0/dist/full.css" rel="stylesheet" type="text/css" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Edit Program Study</title>
+            <div class="items-end">
+                <img src="{{ asset('img/Vector(2).png') }}" alt="">
+            </div>
+        </div>
+    </x-slot>
 
-</head>
-
-<body>
     <div class="hero min-h-screen bg-base-300">
         <div class="hero-content text-center max-w-xl">
-            <div class="card w-96 bg-base-100 text-neutral-content">
+            <div class="card w-96 bg-base-100">
                 <div class="card-body items-center text-center">
-                    <h2 class="card-title">Add Learning Plan</h2>
-                    <form action="" method="">
+                    <form>
                         <div class="form-control w-full max-w-xs">
                             <label class="label">
                                 <span class="label-text">Study Material</span>
@@ -41,6 +41,5 @@
             </div>
         </div>
     </div>
-</body>
 
-</html>
+</x-app-layout>
