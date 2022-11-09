d120789c69af2629a51fd9722f86726e9ea91303
Gazee <100186961+Gazee1112@users.noreply.github.com>
2022-11-09 11:58:14 +0700

    menambahkan controller
    membuat FcultyController dan mengedit card

--- resources/views/faculties/edit.blade.php
+++ resources/views/faculties/edit.blade.php
@@ -9,7 +9,7 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 bg-white border-b border-gray-200 flex justify-center">
-                <div class="card w-96 bg-base-300 text-neutral-content">
+                <div class="card w-96 bg-white text-neutral-content">
                   <div class="card-body items-center text-center">
                     <h2 class="card-title text-neutral">Edit Fakultas</h2>
                     <div class="form-control w-full max-w-xs">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FacultyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,13 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/faculties/edit', function () {
-    return view('faculties.edit');
-});
+Route::resource('faculties/{faculty}/edit', FacultyController::class)->middleware(['auth']);
 
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
