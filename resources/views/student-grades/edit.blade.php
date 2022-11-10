ea8da3c64d25da77e3608abfc86ef52d635e5b2a
Irfan Hanif <14.irfanh@gmail.com>
2022-11-10 14:02:16 +0700

    added component for student-grades.edit

--- resources/views/student-grades/edit.blade.php
+++ resources/views/student-grades/edit.blade.php
@@ -9,7 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                Hello World
+                  <div class="grid grid-cols-2">
+                      <div>
+                        <h2 class="text-lg"><b>Nama Mahasiswa</b></h2>
+                        <p>Status Pengumpulan</p>
+                      </div>
+                      <div class="text-right">
+                        <h2 class="text-lg"><b>100/100</b></h2>
+                        <p>Status Penilaian</p>
+                      </div>
+                  </div> 
                 </div>
             </div>
         </div>
