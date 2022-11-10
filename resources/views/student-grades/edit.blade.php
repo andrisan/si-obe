aba2b5fb72798732c780bcb9a9c6f3fb7d32e2db
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2022-11-10 14:03:24 +0700

    Update edit.blade.php

--- resources/views/student-grades/edit.blade.php
+++ resources/views/student-grades/edit.blade.php
@@ -9,7 +9,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                Hello World
+                        <!-- colapse -->
+                    <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box" id="rubrik">
+                    <div class="collapse-title text-xl font-medium">
+                        <div class="grid md:grid-cols-10 gap-4">
+                        <div class="col-span-8">NO1-1</div>
+                        <button class="btn-sm bg-white text-red-600 rounded-full bg-transparent">clear</button>
+                        <div><input type="text" placeholder="" value="../3" class="input w-full input-sm max-w-sm input-bordered" readonly /></div>
+                        </div>
+                    </div>
+                    <div class="collapse-content">
+                        <p>Mampu menjelaskan konsep pemrograman basis data dalam pengembangan aplikasi</p>
+                        <br />
+                        <div class="btn-group">
+                        <input type="radio" name="options" data-title="baik (3 pts)" class="btn" />
+                        <input type="radio" name="options" data-title="cukup (2 pts)" class="btn" checked />
+                        <input type="radio" name="options" data-title="kurang (1 pts)" class="btn" />
+                        </div>
+                    </div>
+                    </div>
+                    <!-- end colapse -->
                 </div>
             </div>
         </div>
