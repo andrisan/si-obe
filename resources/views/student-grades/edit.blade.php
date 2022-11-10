0d1acacb24a7d0892634c0e04525452a29617dac
Irfan Hanif <14.irfanh@gmail.com>
2022-11-10 17:14:01 +0700

    updated student-grades.edit

--- resources/views/student-grades/edit.blade.php
+++ resources/views/student-grades/edit.blade.php
@@ -22,15 +22,15 @@
                     </div> 
 
                         <!-- colapse -->
-                    <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box" id="rubrik">
-                        <div class="collapse-title text-xl font-medium">
+                    <div tabindex="0" class=" border border-base-300 bg-base-100 rounded-box" id="rubrik">
+                        <div class="text-xl font-medium">
                             <div class="grid md:grid-cols-10 gap-4">
                             <div class="col-span-8">NO1-1</div>
                             <button class="btn-sm bg-white text-red-600 rounded-full bg-transparent">clear</button>
                             <div><input type="text" placeholder="" value="../3" class="input w-full input-sm max-w-sm input-bordered" readonly /></div>
                             </div>
                         </div>
-                        <div class="collapse-content">
+                        <div class="">
                             <p>Mampu menjelaskan konsep pemrograman basis data dalam pengembangan aplikasi</p>
                             <br />
                             <div class="btn-group">
