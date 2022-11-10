f19c545616d45ef2f870b1326b16315755bd3952
Dimas Saputro <89777506+Dimassaputro28@users.noreply.github.com>
2022-11-10 14:42:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -163,6 +163,8 @@ Route::get('/assignments/{assignments}/edit', function () {
 
 Route::get('/Ilos/index', [IloController::class, 'index'])->middleware(['auth']);
 
+Route::get('/Llos/edit', [LloController::class, 'edit'])->middleware(['auth']);
+
 Route::resource('course-classes.assignments', AssignmentController::class)->middleware(['auth']);
 
 Route::resource('syllabi.ilos', IloController::class);
