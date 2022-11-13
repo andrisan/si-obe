8c5d223f59ec787918ec14fd97e0a40756c01046
DanishAlifArsan <101310504+DanishAlifArsan@users.noreply.github.com>
2022-11-13 18:40:46 +0700

    Update edit.blade.php

--- resources/views/student-grades/edit.blade.php
+++ resources/views/student-grades/edit.blade.php
@@ -20,30 +20,29 @@
                             <h2 class="text-lg"><b>100/100</b></h2>
                             <p>Status Penilaian</p>
                         </div>
-                    </div> 
+                        <br>
 
                         <!-- colapse -->
-                    <div tabindex="0" class=" border border-base-300 bg-base-100 rounded-box" id="rubrik">
-                        <div class="text-xl font-medium">
+                    <div tabindex="0" class="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box col-span-2" id="rubrik">
+                        <div class="collapse-title text-xl font-medium">
                             <div class="grid md:grid-cols-10 gap-4">
                             <div class="col-span-8">NO1-1</div>
                             <button class="btn-sm bg-white text-red-600 rounded-full bg-transparent">clear</button>
                             <div><input type="text" placeholder="" value="../3" class="input w-full input-sm max-w-sm input-bordered" readonly /></div>
                             </div>
                         </div>
-                        <div class="">
+                        <div class="collapse-content">
                             <p>Mampu menjelaskan konsep pemrograman basis data dalam pengembangan aplikasi</p>
                             <br />
                             <div class="btn-group">
-                            <input type="radio" name="options" data-title="baik (3 pts)" class="btn" />
-                            <input type="radio" name="options" data-title="cukup (2 pts)" class="btn" checked />
-                            <input type="radio" name="options" data-title="kurang (1 pts)" class="btn" />
+                            <input type="radio" name="options" data-title="baik (3pts)" class="btn px-16" />
+                            <input type="radio" name="options" data-title="cukup (2pts)" class="btn px-16" checked />
+                            <input type="radio" name="options" data-title="kurang (1pts)" class="btn px-16" />
                             </div>
                         </div>
                     </div>
                     <!-- end colapse -->
-
-                  
+                    </div> 
                 </div>
             </div>
         </div>
