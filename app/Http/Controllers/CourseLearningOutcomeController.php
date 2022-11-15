8c0998c4e96fae99923cf63d84776ba31acce726
Rinaldysa <rinaldysa157@gmail.com>
2022-11-16 01:13:48 +0700

    Merge branch 'main' into 21-view-closcreate

--- app/Http/Controllers/CourseLearningOutcomeController.php
+++ app/Http/Controllers/CourseLearningOutcomeController.php
@@ -23,8 +23,12 @@ class CourseLearningOutcomeController extends Controller
      */
     public function create()
     {
+        
         //
     }
+    // button create
+    <input type="button" value="Create" class="btn btn-primary" id="create" onClick="document.location.href='some/page'" />
+    <a href="<?php echo base_url('file controler/functionnya'); ?>" class="btn btn-primary btn-sm floatright">Create</a>
 
     /**
      * Store a newly created resource in storage.
@@ -58,6 +62,9 @@ class CourseLearningOutcomeController extends Controller
     {
         return view('course-learning-outcomes.edit');
     }
+    // button edit
+    <input type="button" value="Edit" class="btn btn-primary" id="create" onClick="document.location.href='some/page'" />
+    <a href="<?php echo base_url('file controler/functionnya'); ?>" class="btn btn-primary btn-sm floatright">Edit</a>
 
     /**
      * Update the specified resource in storage.
@@ -81,4 +88,7 @@ class CourseLearningOutcomeController extends Controller
     {
         //
     }
+    // button delete
+    <input type="button" value="Delete" class="btn btn-primary" id="create" onClick="document.location.href='some/page'" />
+    <a href="<?php echo base_url('file controler/functionnya'); ?>" class="btn btn-primary btn-sm floatright">Delete</a>
 }
