a7f337f46680e624d74447ca6dce99a78c97c8cb
Anas4rif <anas4rif1220@student.ub.ac.id>
2022-11-15 21:11:32 +0700

    Update show.blade.php

--- resources/views/rubrics/show.blade.php
+++ resources/views/rubrics/show.blade.php
@@ -69,7 +69,62 @@
                       </div><hr class="border-black">
                       <p class="font-regular py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, possimus quia nulla error beatae enim saepe odio dolorem facilis hic ipsa esse doloribus in officia, sapiente eum, ducimus aperiam rem.</p>
                     </div>
-                  </div><hr class="border-solid border-black rounded-md mb-2">                  
+                  </div><hr class="border-solid border-black rounded-md mb-2">  
+                  <div class="flex justify-between">
+                    <h1 class="text-[#2E65F3] font-extrabold items-start">N01-2</h1>
+                    <div class="items-end">
+                      <h1 class="font-extrabold text-black">/3</h1>
+                    </div>
+                  </div>
+
+                  <div class="py-5">
+                    <h1 class="text-black text-lg font-bold">Mampu mengimplementasikan konsep dasar php</h1>
+                  </div>
+
+                  <div class="grid grid-cols-3 gap-y-8 pb-10">
+                    <div class="px-5 py-2 w-60 bg-[#AFC7F5] cursor-pointer rounded-md">
+                      <div class="flex justify-between font-bold">
+                        <h1 class="">Baik</h1>
+                        <h1 class="text-sm">3 pts</h1>
+                      </div><hr class="border-black">
+                      <p class="font-regular py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, possimus quia nulla error beatae enim saepe odio dolorem facilis hic ipsa esse doloribus in officia, sapiente eum, ducimus aperiam rem.</p>
+                    </div>
+                    <div class="px-5 py-2 w-60 bg-[#AFC7F5] cursor-pointer rounded-md">
+                      <div class="flex justify-between font-bold">
+                        <h1 class="">Cukup</h1>
+                        <h1 class="text-sm">2 pts</h1>
+                      </div><hr class="border-black">
+                      <p class="font-regular py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, possimus quia nulla error beatae enim saepe odio dolorem facilis hic ipsa esse doloribus in officia, sapiente eum, ducimus aperiam rem.</p>
+                    </div>
+                    <div class="px-5 py-2 w-60 bg-[#AFC7F5] cursor-pointer rounded-md">
+                      <div class="flex justify-between font-bold">
+                        <h1 class="">Kurang</h1>
+                        <h1 class="text-sm">1 pts</h1>
+                      </div><hr class="border-black">
+                      <p class="font-regular py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, possimus quia nulla error beatae enim saepe odio dolorem facilis hic ipsa esse doloribus in officia, sapiente eum, ducimus aperiam rem.</p>
+                    </div>
+                    <div class="px-5 py-2 w-60 bg-[#AFC7F5] cursor-pointer rounded-md">
+                      <div class="flex justify-between font-bold">
+                        <h1 class="">Baik</h1>
+                        <h1 class="text-sm">3 pts</h1>
+                      </div><hr class="border-black">
+                      <p class="font-regular py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, possimus quia nulla error beatae enim saepe odio dolorem facilis hic ipsa esse doloribus in officia, sapiente eum, ducimus aperiam rem.</p>
+                    </div>
+                    <div class="px-5 py-2 w-60 bg-[#AFC7F5] cursor-pointer rounded-md">
+                      <div class="flex justify-between font-bold">
+                        <h1 class="">Cukup</h1>
+                        <h1 class="text-sm">2 pts</h1>
+                      </div><hr class="border-black">
+                      <p class="font-regular py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, possimus quia nulla error beatae enim saepe odio dolorem facilis hic ipsa esse doloribus in officia, sapiente eum, ducimus aperiam rem.</p>
+                    </div>
+                    <div class="px-5 py-2 w-60 bg-[#AFC7F5] cursor-pointer rounded-md">
+                      <div class="flex justify-between font-bold">
+                        <h1 class="">Kurang</h1>
+                        <h1 class="text-sm">1 pts</h1>
+                      </div><hr class="border-black">
+                      <p class="font-regular py-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, possimus quia nulla error beatae enim saepe odio dolorem facilis hic ipsa esse doloribus in officia, sapiente eum, ducimus aperiam rem.</p>
+                    </div>
+                  </div>                
                 </div>
             </div>
         </div>
