a8c1d9a6f64757f5994c095094ec166f95ecaa57
Irfan Hanif <14.irfanh@gmail.com>
2022-11-16 20:46:01 +0700

    edited view for clos.edit

--- resources/views/course-learning-outcomes/edit.blade.php
+++ resources/views/course-learning-outcomes/edit.blade.php
@@ -9,14 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <form action="foo/barr">
+                <form action="/syllabi/{syllabus}/ilos/{ilo}/clos/{clo}">
                     <div class="pb-4">
                         <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Position</strong></label></div>
-                        <input type="text" placeholder="Posisi CLO" class="input input-bordered w-full max-w-xs" /> <br>
+                        <input type="text" placeholder="Posisi CLO" class="input input-bordered w-full max-w-xs" value="{{ $clo->position }}"/> <br>
                     </div>
                     <div class="pb-4">
                     <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Description</strong></label></div>
-                        <textarea class="textarea textarea-bordered w-full" placeholder="Deskripsi CLO"></textarea>
+                        <input type="textarea" class="textarea textarea-bordered w-full" placeholder="Deskripsi CLO" value="{{ $clo->description }}"></textarea>
                     </div>
                 </form>
                 <input type="submit" value="Save" class="btn btn-active rounded-md " />
