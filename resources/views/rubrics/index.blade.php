1db0ba349fa32e244864967dc386618d89028ccd
MuhAtthar <muhatthar@gmail.com>
2022-11-17 21:22:07 +0700

    Create index.blade.php

---
+++ resources/views/rubrics/index.blade.php
@@ -0,0 +1,107 @@
+<x-app-layout>
+  <x-slot name="header">
+    <div class="flex justify-between">
+      <div class="items-start">
+        <h2 class="font-semibold   text-4xl text-gray-800 leading-tight">Rubrics</h2>
+      </div>
+    </div>
+
+  </x-slot>
+
+    <div class="pt-10">
+        <a href="" class="bg-blue-500 ml-[1347px] border-2 border-blue-300 rounded-md shadow-sm px-2.5 sm:px-5 py-2 inline-flex justify-center text-lg font-semibold text-white hover:bg-blue-400 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">+ Rubrik Baru</a>
+    </div>
+    <div class="pt-5">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white border-2 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="overflow-x-auto">
+                        <table class="table mx-auto w-full">
+                            <thead class="text-black">
+                                <tr class="h-10 bg-slate-400">
+                                    <th class="w-16 text-base text-current">No.</th>
+                                    <th class="w-[45rem] text-base text-current">Judul</th>
+                                    <th class="text-center text-base text-current">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-left text-regular">
+                                <tr class="h-10">
+                                    <td>1</td>
+                                    <td>
+                                        <a href="">LK-01</a>
+                                    </td>
+                                    <td class="items-center">
+                                        <div class="flex items-center justify-center">
+                                            <a href="" class="text-blue-700 font-semibold mx-auto text-lg">Edit</a>
+                                            <button href="" class="text-red-600 font-semibold mx-auto text-lg">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="h-10">
+                                    <td>2</td>
+                                    <td>
+                                        <a href="">LK-01</a>
+                                    </td>
+                                    <td class="items-center">
+                                        <div class="flex items-center justify-center">
+                                            <a href="" class="text-blue-700 font-semibold mx-auto text-lg">Edit</a>
+                                            <button href="" class="text-red-600 font-semibold mx-auto text-lg">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="h-10">
+                                    <td>3</td>
+                                    <td>
+                                        <a href="">LK-01</a>
+                                    </td>
+                                    <td class="items-center">
+                                        <div class="flex items-center justify-center">
+                                            <a href="" class="text-blue-700 font-semibold mx-auto text-lg">Edit</a>
+                                            <button href="" class="text-red-600 font-semibold mx-auto text-lg">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="h-10">
+                                    <td>4</td>
+                                    <td>
+                                        <a href="">LK-01</a>
+                                    </td>
+                                    <td class="items-center">
+                                        <div class="flex items-center justify-center">
+                                            <a href="" class="text-blue-700 font-semibold mx-auto text-lg">Edit</a>
+                                            <button href="" class="text-red-600 font-semibold mx-auto text-lg">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="h-10">
+                                    <td>5</td>
+                                    <td>
+                                        <a href="">LK-01</a>
+                                    </td>
+                                    <td class="items-center">
+                                        <div class="flex items-center justify-center">
+                                            <a href="" class="text-blue-700 font-semibold mx-auto text-lg">Edit</a>
+                                            <button href="" class="text-red-600 font-semibold mx-auto text-lg">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr class="h-10">
+                                    <td>6</td>
+                                    <td>
+                                        <a href="">LK-01</a>
+                                    </td>
+                                    <td class="items-center">
+                                        <div class="flex items-center justify-center">
+                                            <a href="" class="text-blue-700 font-semibold mx-auto text-lg">Edit</a>
+                                            <button href="" class="text-red-600 font-semibold mx-auto text-lg">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
