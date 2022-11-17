4061c189c32032c70eb91a4d5428dd31dca6b4cd
adamdaffa56 <89517434+adamdaffa56@users.noreply.github.com>
2022-11-17 14:02:33 +0700

    Update show.blade.php
    menambahkan button tambah, edit, dan delete

--- resources/views/assignments/show.blade.php
+++ resources/views/assignments/show.blade.php
@@ -13,14 +13,19 @@
               <li class="text-blue-600">Assignment</li>
             </ul>
           </div>
-
     </div>
-    <div class="py-5">
-        
+
+    <div class="py-2">
+          <div class="grid">
+            <form action="" class="col-end-9 pb-4 flex ">
+              <button class="btn btn-accent font-bold text-black ">Tambah</button>
+            </form>
+          </div>
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <div class="overflow-x-auto">
+                <div class="overflow-x-auto">   
         <table class="table table-compact w-full">
           <thead>
             <tr>
@@ -29,6 +34,7 @@
               <th>Assigned Date</th> 
               <th>Due Date</th> 
               <th>Note</th> 
+              <th>Opsi</th>
             </tr>
           </thead> 
           <tbody>
@@ -40,6 +46,17 @@
               <td>{{$as->assigned_date}}</td>
               <td>{{$as->due_date}}</td>
               <td>{{$as->note}}</td>
+              <td>
+                <div class="flex items-strecth">
+                  <form action="" method="get">
+                    <button class="btn btn-warning btn-sm m-1" value="">Edit</button>
+                  </form>
+                  <form action="" method="get">
+                    <button class="btn btn-error btn-sm m-1" value=""
+                      onclick="return confirm('Yakin ingin menghapus data ?');">Delete</button>
+                  </form>
+                </div>
+              </td>
             </tr>
             <?php $i++ ?>
             @endforeach
