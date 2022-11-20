e4e6b026953799e347cf324656310fa5cf3c6bb9
Irfan Hanif <14.irfanh@gmail.com>
2022-11-20 11:48:51 +0700

    edited and finished edit view

--- resources/views/course-learning-outcomes/edit.blade.php
+++ resources/views/course-learning-outcomes/edit.blade.php
@@ -9,19 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <form action="{{ route(syllabi.ilos.clos.update) }}" method="post">
+                <form action="{{ route('syllabi.ilos.clos.update', [$syllabus, $ilo, $clo]) }}" method="post">
+                    @csrf
+                    @method('patch')
                     <div class="pb-4">
                         <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Position</strong></label></div>
-                        <input type="text" placeholder="Posisi CLO" class="input input-bordered w-full max-w-xs" value="{{ $clo->position }}"/> <br>
+                        <input type="text" name="position" placeholder="Position" class="input input-bordered w-full max-w-xs" value="{{ old('position', $clo->position) }}"/> <br>
                     </div>
                     <div class="pb-4">
                     <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Description</strong></label></div>
-                        <input type="textarea" class="textarea textarea-bordered w-full" placeholder="Deskripsi CLO" value="{{ $clo->description }}"></textarea>
+                        <textarea class="textarea textarea-bordered w-full" placeholder="Description" name="description" >{{ old('description', $clo->description) }}</textarea>
                     </div>
                     <input type="submit" value="Save" class="btn btn-active rounded-md " />
-                </form>
-                <form action="{{ route(syllabi.ilos.clos.update) }}">
-                    <button class="btn btn-outline rounded-md">Cancel</button>
+                    <a href="{{ route('syllabi.ilos.clos.index', [$syllabus, $ilo]) }}" class="btn btn-outline rounded-md">Cancel</a>
                 </form>
                 </div>
             </div>
