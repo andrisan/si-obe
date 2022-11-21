cb550bca76fe2739a0540cbec4f30d174f49b79b
RazzaaqZul <razzaaqzulkahfi123@gmail.com>
2022-11-21 22:22:50 +0700

    Update index.blade.php
    add thumbnail

--- resources/views/assignments/index.blade.php
+++ resources/views/assignments/index.blade.php
@@ -20,74 +20,57 @@
         </div>
 
     </x-slot>
-    <!--     
-    <div class="pb-5">
-        <div class="text-sm breadcrumbs pl-8 pt-5 font-bold text-gray-600">
-            <ul>
-                <li><a href="">Dashboard</a></li>
-                <li><a href="">Course</a></li>
-                {{-- <li><a href="">{{ $assignment->find(1)->course_class_id }}</a></li> --}}
-                <li><a href="">{{ $courseClass->name }}</a></li>
-                <li class="text-blue-600">Assignment</li>
-            </ul>
-        </div>
-
-    </div> -->
     <div class="py-5">
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="overflow-x-auto">
-                        <div class=" bg-no-repeat rounded-xl bg-right"
-                            style="background-image: url(https://c4.wallpaperflare.com/wallpaper/98/544/886/flash-drive-wallpaper-preview.jpg); background-color: red;">
+                    <!--  -->
+                    <div class="bg-center bg-no-repeat bg-cover rounded-xl "
+                        style="background-image: url({{ $courseClass->thumbnail_img}}); position:relative; ">
 
-                            <div class="hero-overlay bg-opacity-60"></div>
-                            <div class="text-left text-neutral-content py-28 px-5">
-                                <div class="">
-                                    <h1 class="mt-20 text-5xl font-bold mb-3 ">{{ $courseClass->name }}</h1>
-                                    <p class="mb-[-50px]"> TI - A | Jumat, 07:00 </p>
+                        <div class="py-32 px-5 text-neutral-content align-text-center">
+                            <div class="max-w-md ">
+                                <h1 class=" text-5xl font-bold  " style="position:absolute; bottom:12px; left:15px;">
+                                    {{ $courseClass->name }}</h1>
 
-
-                                </div>
                             </div>
                         </div>
-
-
                     </div>
+
 
                     <div class=" mt-10 border-2 border-gray-200 shadow-md rounded-2xl bg-gray-100	 ">
 
-                        <div class="hero-overlay bg-opacity-60">
+                        <a href="/course-classes/{course-class}/assignments/create">
+                            <div class="text-left text-neutral-content py-3 px-5 hover:bg-gray-200">
+
+                                <div class="flex flex-row gap-5">
+                                    <div class="flexke1 ">
 
 
-
-                        </div>
-                        <div class="text-left text-neutral-content py-6 px-6 hover:bg-gray-200">
-
-                            <div class="flex flex-row gap-6">
-                                <div class="flexke1 ">
-                                    <a href="/course-classes/{course-class}/assignments/create
-">
                                         <button class="btn btn-circle btn-outline hover:-translate hover:scale-125">
                                             <img src="https://cdn-icons-png.flaticon.com/512/1089/1089316.png?w=740&t=st=1668966431~exp=1668967031~hmac=24ef8490194d8710c68796314f0efdef3096981619966e5a9afb9b4cf2abfd04"
                                                 alt="" class="w-6">
 
                                         </button>
-                                    </a>
-                                </div>
 
-                                <div class="flexke2 pt-3 ">
+                                    </div>
 
-                                    <div class="truncate">
-                                        <h1 class="truncate text-xl font-bold text-black hover:text-orange-600">Tambah
-                                            Assignment
-                                        </h1>
+                                    <div class="flexke2 pt-3 ">
+
+                                        <div class="truncate">
+                                            <h1 class="truncate text-lg font-bold text-black hover:text-orange-600">
+                                                Tambah
+                                                Assignment
+                                            </h1>
+
+                                        </div>
 
                                     </div>
                                 </div>
                             </div>
-                        </div>
+
+                        </a>
                     </div>
 
                     <?php $i = 1 ?>
@@ -101,7 +84,7 @@
 
                         </div>
                         <a href="assignments/{{ $as->id }}">
-                            <div class="text-left text-neutral-content py-7 px-5">
+                            <div class="text-left text-neutral-content py-3 px-5">
                                 <div class="flex flex-row gap-5">
                                     <div class="logo">
                                         <button class="btn btn-circle  ">
