c391f979bcea79688a996c3d8bfed829cfcd018f
reyhanmichiels <michiels.reyhan@gmail.com>
2022-11-24 01:24:37 +0700

    Update index.blade.php

--- resources/views/rubrics/index.blade.php
+++ resources/views/rubrics/index.blade.php
@@ -12,7 +12,9 @@
         <div class="flex flex-row sm:justify-end px-3 sm:px-16 -mr-2 sm:-mr-3">
             <div class="order-5 sm:order-6 mr-2 sm:mr-3">
                 <div class="relative">
-                    <a href="{{ route('rubrics.create') }}" class="bg-blue-500 border-2 border-blue-300 rounded-md shadow-sm px-2.5 sm:px-5 py-2 inline-flex justify-center text-lg font-semibold text-white hover:bg-blue-400 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">+ Rubric Baru</a>
+                    <a href="{{ route('rubrics.create') }}"
+                        class="bg-blue-500 border-2 border-blue-300 rounded-md shadow-sm px-2.5 sm:px-5 py-2 inline-flex justify-center text-lg font-semibold text-white hover:bg-blue-400 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">+
+                        Rubric Baru</a>
                 </div>
             </div>
         </div>
@@ -20,7 +22,7 @@
 
     <div>
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white border-2 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white border-2 overflow-hidden shadow-sm sm:rounded-lg mb-5">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="overflow-x-auto">
                         <table class="table mx-auto w-full">
@@ -33,22 +35,22 @@
                             </thead>
                             <tbody class="text-left text-regular">
                                 <?php $i = 1 ?>
-                                @foreach ($rubric as $rubric)
+                                @foreach ($rubric as $rub)
                                 <tr class="h-10">
                                     <td>{{ $i }}</td>
                                     <td>
-                                        <a href="{{ route('rubrics.show', $rubric) }}">{{ $rubric->title }}</a>
+                                        <a href="{{ route('rubrics.show', $rub) }}">{{ $rub->title }}</a>
                                     </td>
                                     <td class="items-center">
                                         <div class="flex items-center justify-center">
-                                            <a href="{{ route('rubrics.edit', $rubric) }}"
+                                            <a href="{{ route('rubrics.edit', $rub) }}"
                                                 class="text-blue-700 font-semibold mx-auto text-lg">Edit</a>
-                                            <form action="{{ route('rubrics.destroy', $rubric) }}" method="post">
+                                            <form action="{{ route('rubrics.destroy', $rub) }}" method="post">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button href=""
                                                     class="text-red-600 font-semibold mx-auto text-lg cursor-pointer"
-                                                    @click="confirm('Are you sure?') ? console.log('asdasd') : false">Delete</button>
+                                                    onclick="event.preventDefault(); confirm('Are you sure?') && this.closest('form').submit();">Delete</button>
                                             </form>
                                         </div>
                                     </td>
@@ -56,11 +58,11 @@
                             </tbody>
                             <?php $i += 1 ?>
                             @endforeach
-
                         </table>
                     </div>
                 </div>
             </div>
+            {{ $rubric->links() }}
         </div>
     </div>
 </x-app-layout>
