e6c47e2866e690598d80263762abb56e0f74b2ad
A. Fauzi <100508741+Aither-24@users.noreply.github.com>
2022-11-23 22:53:12 +0700

    Update index.blade.php

--- resources/views/learning-plans/index.blade.php
+++ resources/views/learning-plans/index.blade.php
@@ -12,16 +12,42 @@
         <a href="{{ route('syllabi.learning-plans.create', [$syllabi]) }}" class="w-full bg-white border border-gray-300 rounded-md shadow-sm px-2.5 sm:px-4 py-2 inline-flex justify-center text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <span class="pr-1">New Learning Plan</span>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 612.001 612.001" style="enable-background:new 0 0 612.001 612.001;" xml:space="preserve">
+            viewBox="0 0 612.001 612.001" style="enable-background:new 0 0 612.001 612.001;" xml:space="preserve">
+            <g>
+              <g>
+                <g>
+                  <path d="M567.734,57.47c-28.593-28.593-66.607-44.338-107.044-44.338c-40.435,0-78.451,15.745-107.041,44.338
+                  c-28.593,28.593-44.34,66.606-44.34,107.044c0,40.435,15.747,78.449,44.34,107.041c28.592,28.593,66.606,44.341,107.041,44.341
+                  c40.436,0,78.452-15.748,107.044-44.341C626.757,212.532,626.757,116.494,567.734,57.47z M543.697,247.519
+                  c-22.171,22.174-51.652,34.384-83.007,34.384c-31.356,0-60.833-12.21-83.004-34.384c-22.173-22.171-34.383-51.649-34.383-83.004
+                  s12.21-60.836,34.383-83.007c22.171-22.171,51.65-34.382,83.004-34.382c31.356,0,60.836,12.21,83.007,34.382
+                  C589.466,127.275,589.466,201.75,543.697,247.519z" />
+                  <path d="M508.379,146.853h-32.682v-32.681c0-9.387-7.61-16.996-16.996-16.996s-16.996,7.609-16.996,16.996v32.681h-32.682
+                  c-9.386,0-16.996,7.61-16.996,16.996c0,9.389,7.61,16.996,16.996,16.996h32.682v32.683c0,9.389,7.61,16.996,16.996,16.996
+                  s16.996-7.607,16.996-16.996v-32.683h32.682c9.386,0,16.996-7.607,16.996-16.996
+                  C525.375,154.463,517.766,146.853,508.379,146.853z" />
+                  <path d="M361.408,470.254c-23.202-16.36-50.76-28.432-80.75-35.675c27.815-24.092,45.865-62.632,45.865-105.984
+                  c0-72.819-50.922-132.062-113.514-132.062c-62.589,0-113.512,59.242-113.512,132.062c0,43.353,18.05,81.893,45.865,105.987
+                  c-29.988,7.239-57.546,19.312-80.748,35.672C22.947,499.633,0,539.273,0,581.874c0,9.386,7.61,16.996,16.996,16.996h392.027
+                  c9.387,0,16.996-7.61,16.996-16.996C426.02,539.273,403.073,499.633,361.408,470.254z M326.523,564.878v-30.355
+                  c0-9.386-7.61-16.996-16.996-16.996s-16.996,7.61-16.996,16.996v30.355H133.49v-30.355c0-9.386-7.61-16.996-16.996-16.996
+                  s-16.996,7.61-16.996,16.996v30.355H35.77c5.249-24.922,21.989-48.196,48.432-66.841c34.183-24.102,79.929-37.376,128.81-37.376
+                  s94.627,13.272,128.807,37.376c26.443,18.645,43.183,41.918,48.43,66.841H326.523z M133.49,328.596
+                  c0-54.076,35.673-98.07,79.52-98.07c43.848,0,79.522,43.996,79.522,98.07c0,54.079-35.673,98.073-79.52,98.073
+                  S133.49,382.673,133.49,328.596z" />
+                </g>
+              </g>
+            </g>
           </svg>
         </a>
       </div>
     </div>
     <div class="mb-5 overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
+      @if($learningPlans->isNotEmpty())
       <table class="border-collapse table-auto w-full bg-white table-striped relative">
         <thead>
           <tr class="text-left">
-            <th class="bg-gray-50 sticky top-0 border-b border-gray-100 px-6 py-3 text-gray-500 font-bold tracking-wider uppercase text-xs truncate">No</th>
-            <th class="bg-gray-50 sticky top-0 border-b border-gray-100 px-6 py-3 text-gray-500 font-bold tracking-wider uppercase text-xs truncate w-64">Week Number</th>
+            <th class="bg-gray-50 sticky top-0 border-b border-gray-100 px-6 py-3 text-gray-500 font-bold tracking-wider uppercase text-xs truncate w-16">Week</th>
             <th class="bg-gray-50 sticky top-0 border-b border-gray-100 px-6 py-3 text-gray-500 font-bold tracking-wider uppercase text-xs truncate w-16">LLO</th>
             <th class="bg-gray-50 sticky top-0 border-b border-gray-100 px-6 py-3 text-gray-500 font-bold tracking-wider uppercase text-xs truncate w-128">Study Material</th>
             <th class="bg-gray-50 sticky top-0 border-b border-gray-100 px-6 py-3 text-gray-500 font-bold tracking-wider uppercase text-xs truncate w-128">Learning Method</th>
@@ -32,7 +58,6 @@
         <tbody>
           @foreach ($learningPlans as $learningPlan)
           <tr>
-            <td class="text-gray-600 px-6 py-3 border-t border-gray-100">{{ $loop->index + $learningPlans->firstItem() }}</td>
             <td class="text-gray-600 px-6 py-3 border-t border-gray-100">{{ $learningPlan->week_number }}</td>
             <td class="text-gray-600 px-6 py-3 border-t border-gray-100">{{ $learningPlan->llo_id }}</td>
             <td class="text-gray-600 px-6 py-3 border-t border-gray-100">{{ $learningPlan->study_material }}</td>
@@ -40,8 +65,8 @@
             <td class="text-gray-600 px-6 py-3 border-t border-gray-100">{{ $learningPlan->estimated_time }}</td>
             <td class="text-gray-600 px-6 py-3 border-t border-gray-100">
               <div class="flex flex-wrap space-x-4">
-                <a href="{{ route('syllabi.learning-plans.edit', [$syllabi, $learningPlans]) }}" class="text-blue-500">Edit</a>
-                <form method="POST" action="{{ route('syllabi.learning-plans.destroy', [$syllabi, $learningPlans]) }}">
+                <a href="" class="text-blue-500">Edit</a>
+                <form method="POST" action="">
                   @csrf
                   @method('delete')
 
@@ -56,49 +81,11 @@
           @endforeach
         </tbody>
       </table>
+      @else
+      <br>
+      <h1 class="text-2xl font-extrabold text-center" style="font-weight: 900;">Data tidak ditemukan</h1>
+      <br>
+      @endif
     </div>
-    {{ $learningPlans->links() }}
   </div>
-  <!-- <table class="table-fixed w-full">
-    @if($learningPlans->isNotEmpty())
-    <thead>
-      <tr class="bg-[#F7F7F9] border-2 h-10">
-        <th class="w-auto">Learning Plan ID</th>
-        <th class="w-auto">Syllabus ID</th>
-        <th class="w-auto">Week Number</th>
-        <th class="w-auto">LLO's ID</th>
-        <th class="w-auto">Study Material</th>
-        <th class="w-auto">Learning Method</th>
-        <th class="w-auto">Est. Time</th>
-        <th class="w-auto">Created at</th>
-        <th class="w-auto">Updated at</th>
-        <th>Aksi</th>
-      </tr>
-    </thead>
-    <tbody class="text-center border-2 border-black text-black">
-      @foreach($learningPlans as $learningPlan)
-      <tr class="border-2 h-14">
-        <td>{{ $learningPlan->id }}</td>
-        <td>{{ $learningPlan->syllabus_id }}</td>
-        <td>{{ $learningPlan->week_number }}</td>
-        <td>{{ $learningPlan->llo_id }}</td>
-        <td>{{ $learningPlan->study_material }}</td>
-        <td>{{ $learningPlan->learning_method }}</td>
-        <td>{{ $learningPlan->estimated_time }}</td>
-        <td>{{ $learningPlan->created_at }}</td>
-        <td>{{ $learningPlan->updated_at }}</td>
-        <td>
-          <button href="`learning-plans/${row.id}/edit`" class="btn btn-warning btn-sm">Edit</button>
-          <button class="btn btn-error btn-sm" @click="confirm('Are you sure?') ? console.log('asdasd') : false">Delete</button>
-        </td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
-  @else
-  <br>
-  <h1 class="text-2xl font-extrabold text-center" style="font-weight: 900;">Data tidak ditemukan</h1>
-  <br>
-  @endif -->
-
 </x-app-layout>
