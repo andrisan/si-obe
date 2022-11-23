4f9c79c3908ba0ae3a6c6d3be3d5ff0950056a9e
reyhanmichiels <michiels.reyhan@gmail.com>
2022-11-23 12:52:33 +0700

    Update show.blade.php

--- resources/views/rubrics/show.blade.php
+++ resources/views/rubrics/show.blade.php
@@ -17,22 +17,22 @@
           <hr class="border-solid border-black rounded-md mb-2">
           
           {{-- loop start --}}
-          @foreach ($rubric->criteria as $criteria)
+          @foreach ($rubric->criterias as $criterias)
           <div class="flex justify-between">
-            <h1 class="text-[#2E65F3] font-extrabold items-start">{{ $criteria->title }}</h1>
+            <h1 class="text-[#2E65F3] font-extrabold items-start">{{ $criterias->title }}</h1>
             <div class="items-end">
-              <h1 class="font-extrabold text-black">/{{ $criteria->max_point }}</h1>
+              <h1 class="font-extrabold text-black">/{{ $criterias->max_point }}</h1>
             </div>
           </div>
 
           <div>
             <div class="py-5">
-              <h1 class="text-black text-lg font-bold">{{ $criteria->description }}</h1>
+              <h1 class="text-black text-lg font-bold">{{ $criterias->description }}</h1>
             </div>
 
             <!-- <div class="flex space-x-40 justify-center text-black underline-offset-2"> -->
               <div class="grid grid-cols-3 gap-y-8 pb-10">
-              @foreach ($criteria->criteriaLevels as $cl)
+              @foreach ($criterias->criteriaLevels as $cl)
               <div class="px-5 py-2 w-60 bg-[#AFC7F5] cursor-pointer rounded-md">
                 <div class="flex justify-between font-bold">
                   <h1 class="">{{ $cl->title }}</h1>
