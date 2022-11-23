c5fd04ecac329a24bfbd8795e54cc4cac7c88ac7
A. Fauzi <100508741+Aither-24@users.noreply.github.com>
2022-11-23 19:29:40 +0700

    create model

--- app/Models/LearningPlan.php
+++ app/Models/LearningPlan.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class LearningPlan extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    public function syllabus()
+    {
+        return $this->belongsTo(Syllabus::class);
+    }
+
+    public function lessonLearningOutcome()
+    {
+        return $this->belongsTo(LessonLearningOutcome::class);
+    }
 }
