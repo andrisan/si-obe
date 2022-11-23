df32a0013f9d1760711dc9a435ffdb1b6f3a8d22
MuhAtthar <muhatthar@gmail.com>
2022-11-23 19:55:06 +0700

    Update edit.blade.php

--- resources/views/rubrics/edit.blade.php
+++ resources/views/rubrics/edit.blade.php
@@ -16,7 +16,7 @@
     <div class="py-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-10 bg-white rounded-md border-2 border-black  ">
+                <div class="p-10 bg-white rounded-md border-2">
                     <div class="">
                         <h1 class=" text-black text-3xl">
                             Masukkan Judul Baru
