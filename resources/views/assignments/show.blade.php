f362c09c03073ad939c561f28809ead59b724081
M. Rezky Revansyah Suprihono <103492921+rezkyrevansyah@users.noreply.github.com>
2022-11-23 17:41:14 +0700

    menambahkan show

--- resources/views/assignments/show.blade.php
+++ resources/views/assignments/show.blade.php
@@ -1,68 +1,47 @@
 <x-app-layout>
   <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Assignment Kelas')}}
-      </h2>
-  </x-slot>
-  <div class="pb-5">
-      <div class="text-sm breadcrumbs pl-8 pt-5 font-bold text-gray-600">
-          <ul>
-            <li><a href="">Dashboard</a></li>
-            <li><a href="">Course</a></li>
-            <li><a href="">{{ $courseClass->id }}</a></li>
-            <li class="text-blue-600">Assignment</li>
-          </ul>
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Assignment Kelas')}}
+    </h2>
+</x-slot>
+<br>
+
+<div class="py-12">
+<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200 flex ">
+            <div class="text-2xl font-bold ">
+
+                <h1>Tugas Pertemuan 1</h1>
+            </div>
         </div>
-  </div>
 
-  <div class="py-2">
+        <div class="footer items-center p-4 bg-neutral text-neutral-content">
+            <div class="items-center grid-flow-col">
+                <svg width="36" height="36" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+                <p>Assigned Date : 08 Nov</p>
+            </div>
+            <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+                <p>Due Date: 17 Nov 11.59</p>
+            </div>
+        </div>
 
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 bg-white border-b border-gray-200">
-                <div class="overflow-x-auto">   
-                  <table class="table table-compact w-full">
-                    <thead>
-                      <tr>
-                        <th>No.</th> 
-                        <th>Assignment</th> 
-                        <th>Assigned Date</th> 
-                        <th>Due Date</th> 
-                        <th>Note</th> 
-                        <th>Opsi</th>
-                      </tr>
-                    </thead> 
-                    <tbody>
-                      <?php $i = 1 ?>
-                      <tr>
-                        <td>{{ $i }}</td>
-                        <td>{{ $assignment->assignmentPlan->title ??null }}</td>
-                        <td>{{ $assignment->assigned_date??null }}</td>
-                        <td>{{ $assignment->due_date??null }}</td>
-                        <td>{{ $assignment->note }}</td>
-                        <td>
-                          <div class="flex items-strecth">
-                            <form action="{{ route('course-classes.assignments.edit', [$courseClass, $assignment]) }}" method="get">
-                              <button class="btn btn-warning btn-sm m-1" value="">Edit</button>
-                            </form>
-                            <form method="POST" action="{{ route('course-classes.assignments.destroy', [$courseClass, $assignment]) }}">
-                              @csrf
-                              @method('delete')
+        <div class="p-6 bg-white border-b border-gray-200 flex ">
+            <div class="text-2xl font-bold ">
+                <h1>Modul 1: Belajar gamon (gamau move on)</h1>
+            </div>
+        </div>
 
-                              <button class="btn btn-error btn-sm m-1"
-                                  onclick="event.preventDefault(); confirm('Yakin ingin menghapus data?') && this.closest('form').submit();">
-                                  {{ __('Delete') }}
-                              </button>
-                            </form>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody> 
-                  </table>
+            <div class="tombol justify-end flex flex-row gap-2 py-5 px-10">
+                <form action="" method="get">
+                    <button class="btn btn-outline btn-warning w-24" value="">Edit</button>
+                </form>
+                <form action="" method="get">
+                    <button class="btn btn-active btn-error w-24" value=""
+                    onclick="return confirm('Yakin ingin menghapus data ?');">Delete</button>
+                </form>
                 </div>
-              </div>
-          </div>
-          <a class="btn btn-primary btn-sm m-4" href="{{ route('course-classes.assignments.index', [$courseClass, $assignment]) }}"> <- Back </a>
-      </div>
-  </div>
+                <a class="btn btn-primary btn-sm m-4 " href="{{ route('course-classes.assignments.index', [$courseClass, $assignment]) }}"> <- Back </a>
+            </div>
+    </div>
 </x-app-layout>
