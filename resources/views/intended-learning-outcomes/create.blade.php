75b1d295f7f1ac406369799c8f36357684645ed3
Moch Danial Rasyid Akbar S <danialrasyid1@student.ub.ac.id>
2022-11-23 22:28:17 +0700

    Update create.blade.php

--- resources/views/intended-learning-outcomes/create.blade.php
+++ resources/views/intended-learning-outcomes/create.blade.php
@@ -15,15 +15,16 @@
                             <h2 class="font-semibold text-3xl text-center">Masukkan Capaian Pembelajaran Baru Anda</h2>
                             </div>
                         </div>
-                    </div>  
+                    </div>
                     <div class="flex items-left justify-left">
-                        <form action="/syllabi/blabla/ilos" method="GET">
+                        <form action="{{route('syllabi.ilos.store',[$syllabus])}}" method="post">
+                            @csrf
                             <h3 class="font-semibold text-md lg:ml-14 pt-16 tracking-widest ml-4 lg:text-lg iloPosition">POSISI</h3>
-                            <input type="text" placeholder="Ketik di sini..." class="input input-bordered font-regular lg:ml-14 lg:w-[30vw] text-base w-[40vw] ml-4 border-[3px] border-solid bg-white mt-4" />
+                            <input type="text" name ="position" id="position" placeholder="Ketik di sini..." class="input input-bordered font-regular lg:ml-14 lg:w-[30vw] text-base w-[40vw] ml-4 border-[3px] border-solid bg-white mt-4" />
                             <h3 class="font-semibold text-md pt-4 tracking-widest ml-4 lg:ml-14 lg:text-lg iloDesc">DESKRIPSI</h3>
-                            <textarea class="textarea textarea-bordered w-[70vw] ml-4 lg:ml-14 border-[3px] border-solid bg-white mt-4 h-[266px] resize-none font-regular text-base" placeholder="Ketik di sini..."></textarea>
+                            <input class="textarea textarea-bordered w-[70vw] ml-4 lg:ml-14 border-[3px] border-solid bg-white mt-4 h-[266px] resize-none font-regular text-base" name="description" id="description" placeholder="Ketik di sini...">
                             <br>
-                            <input type="submit" value="Submit" class="btn btn-primary lg:text-lg w-[150px] mt-9 text-capitalize bg-[#2E65F3] border-none transition-all duration-300 ml-4 hover:bg-sky-700 lg:ml-14">
+                            <button type="submit"  name="submit" id="submit" class="btn btn-primary lg:text-lg w-[150px] mt-9 text-capitalize bg-[#2E65F3] border-none transition-all duration-300 ml-4 hover:bg-sky-700 lg:ml-14">submit</button>
                         </form>
                     </div>
                 </div>
