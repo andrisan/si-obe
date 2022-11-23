36bd0a597c4f9274389884bfc859e51260c9dc17
A. Fauzi <100508741+Aither-24@users.noreply.github.com>
2022-11-23 22:12:05 +0700

    Update LearningPlan.php

--- app/Models/LearningPlan.php
+++ app/Models/LearningPlan.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Syllabus;
+use App\Models\LessonLearningOutcome;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,8 +11,16 @@ class LearningPlan extends Model
     use HasFactory;
 
     public $timestamps = false;
+    protected $fillable = [
+        'Week Number',
+        'LLO_ID',
+        'studyMaterial',
+        'learningMethod',
+        'estTime',
+        'updatedAt',
+    ];
 
-    public function syllabus()
+    public function syllabi()
     {
         return $this->belongsTo(Syllabus::class);
     }
