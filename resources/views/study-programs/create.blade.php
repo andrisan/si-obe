69e5f2d0896a0f36c1e5e239e9e9530eddbe6182
asyamadil2 <asyamadil@gmail.com>
2022-11-23 18:46:03 +0700

    Update create.blade.php

--- resources/views/study-programs/create.blade.php
+++ resources/views/study-programs/create.blade.php
@@ -19,23 +19,19 @@
         </div>
 
         <!--Awal Form Create-->
+        
         <div class="flex flex-row justify-center">
             <div class="block p-6 rounded-lg shadow-2xl bg-white max-w-md basis-1/2">
-                <form>
-                    <div class="form-group mb-6">
-                    <label for="exampleInputEmail1" class="form-label inline-block mb-2 text-gray-700">
-                        Department
-                    </label>
-                    <input type="text" class="form-control block w-full px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-400 rounded" 
-                        id="DeptName" placeholder="Name">
-                    </div>
-                    <div class="form-group mb-6">
-                    <label for="exampleInputPassword1" class="form-label inline-block mb-2 text-gray-700">
-                        Study Program
-                    </label>
-                    <input type="text" class="form-control block w-full px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-400 rounded" 
-                        id="SPName" placeholder="Name">
-                    </div>
+                <form method="POST" action="{{ route('faculties.departments.study-programs.store', [$faculty, $department]) }}">
+                    @csrf
+                        <div class="">
+                            <label for="exampleInputPassword1" class="form-label inline-block mb-2 text-gray-700">
+                                Study Program
+                            </label>
+                            <input type="text" class="form-control block w-full px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-400 rounded" 
+                                id="SPName" placeholder="Name" name="name">
+                                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
 
                     <div class="mt-5">
                         <button type="submit" class="btn btn-primary px-6 py-2.5 text-white font-medium text-xs rounded shadow-md">
