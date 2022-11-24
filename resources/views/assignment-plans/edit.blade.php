2090ab3d90d1eac9eed54de4daf9c17e417ddbe2
putriraiss <putridaliana@gmail.com>
2022-11-24 13:20:53 +0700

    Update edit.blade.php

--- resources/views/assignment-plans/edit.blade.php
+++ resources/views/assignment-plans/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit Assignment Plans') }}
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
@@ -9,78 +9,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-
-                <form action="{{ route('syllabi.assignment-plans.update', [$syllabus, $plan]) }}"  method="post">
-                        @csrf
-                        @method('patch')
-                  
+                <form action="{{ route('syllabi.assignment-plans.update', [$syllabus, $plan]) }}" method="post">
+                @csrf
+                @method('patch')
                         <div class="container p-4 text-primary mt-2 text-sm">
-                            
-                                <label class="uppercase font-bold  text-blue-600" for="fname">ID</label><br>
-                                <input type="text" class="input input-bordered input-ghost input-l w-full max-w-xl mb-2 mt-2" 
-                                name="id" id="id"value="{{$plan->id}}"/>
-                            
+                                <label class="uppercase font-bold  text-blue-600" for="name">ID</label><br>
+                                <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="id"/> <br>
                         </div>
-
                         <div class="container p-4 text-primary text-sm">
-                           
-                                <label class="uppercase font-bold  text-blue-600" for="fname">Title</label><br>
-                                <input type="text" class="input input-bordered input-ghost input-l w-full max-w-xl mb-4 mt-2" name="title" id="title" value="{{$plan->title}}"/>
-                            
+                                <label class="uppercase font-bold  text-blue-600" for="name">Title</label><br>
+                                <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="title"/> <br>
                         </div>
-
                         <div class="container p-4 text-primary text-sm">
-                            
-                                <label class="uppercase font-bold  text-blue-600" for="fname">Description</label><br>
-                                <input type="text"  class="input input-bordered input-ghost input-l w-full max-w-xl mb-4 mt-2" name="description" id="description" value="{{$plan->description}}" />
-                            
-                        </div>
-
+                                <label class="uppercase font-bold  text-blue-600" for="name">Description</label><br>
+                                <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="description"/> <br>
+                         </div>
+                         <div class="container p-4 text-primary text-sm">
+                                <label class="uppercase font-bold  text-blue-600" for="name">Created at</label><br>
+                                <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="created_at"/> <br>
+                         </div>
+                         <div class="container p-4 text-primary text-sm">
+                                <label class="uppercase font-bold  text-blue-600" for="name">Updated at</label><br>
+                                <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="updated_at"/> <br>
+                         </div>
                         <div class="container p-4 text-primary text-sm">
-                            
-                                <label class="uppercase font-bold  text-blue-600" for="fname">Created at</label><br>
-                                <input type="text" class="input input-bordered input-ghost input-l w-full max-w-xl mb-4 mt-2" name="created_at" id="created_at" value="{{$plan->created_at}}" />
-                            
+                                <label class="uppercase font-bold  text-blue-600" for="name">Assignment style</label><br>
+                                <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="assignment_style"/> <br>
                         </div>
                         
-                        <div class="container p-4 text-primary text-sm">
-                            
-                                <label class="uppercase font-bold  text-blue-600" for="fname">Updated_at</label><br>
-                                <input type="text"  class="input input-bordered input-ghost input-l w-full max-w-xl mb-4 mt-2" name="updated_at" id="updated_at" value="{{$plan->updated_at}}"/>
-                            
-                        </div>
-                     
-                        <div class="container p-4 text-primary text-sm">
-                           
-                                <label class="uppercase font-bold  text-blue-600" for="fname">Assignment style</label><br>
-                                <input type="text"  class="input input-bordered input-ghost input-l w-full max-w-xl mb-4 mt-2" name="assignment_style"  id="assignment_style" value="{{$plan->assignment_style}}" />
-                            
-                        </div>
-                    
-
-
-                       <!--
-                        <div class="mt-4 ml-4">
-                            {{--<form action="{{route('assignment-plans.index'}}">--}}
                         
-                            <button class="bg-blue-300 hover:bg-blue-600 hover:text-white text-white font-bold py-2 px-4 rounded border-outline border-2" href="{{route('assignment-plans.store')}}">
-                                Create
-                            </button>
-                            {{--</form>}}
-
-                            {{--<form action="{{route('assignment-plans.index'}}">--}}
-
-                            <button class="bg-error hover:bg-red-600 hover:text-white text-white font-bold py-2 px-4 rounded border-outline border-2" href="{{route('courses.index')}}">
-                                Cancel
-                            </button>
-                            </form>
-                       
-                        </div>
-                        !-->
-
-                        <input type="submit" value="Save" class="bg-blue-300 hover:bg-blue-600 hover:text-white text-white font-bold py-2 px-4 rounded border-outline border-2"/>
-                        <a href="{{ route('syllabi.assignment-plans.index', [$syllabus]) }}" class="bg-error hover:bg-red-600 hover:text-white text-white font-bold py-2 px-4 rounded border-outline border-2">Cancel</a>
-                </form>    
+                        <input type="submit" value="Save" class="btn btn-active rounded-md " />
+                        <a href="{{ route('syllabi.assignment-plans.index', [$syllabus]) }}" class="btn btn-outline rounded-md">Cancel</a>
+                     </form> 
                 </div>
             </div>
         </div>
