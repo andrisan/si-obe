30c2c8c0fa52867417ac32d85d4b946a63086ec3
Tanziil <100509407+Tanziil20@users.noreply.github.com>
2022-11-24 15:16:46 +0700

    Update edit.blade.php

--- resources/views/learning-plans/edit.blade.php
+++ resources/views/learning-plans/edit.blade.php
@@ -9,27 +9,40 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{ route('syllabi.learning-plans.update', [$syllabi, $learningPlan]) }}" method="post">
+                    <form action="{{ route('syllabi.learning-plans.update', [$syllabus, $plan]) }}" method="post">
                         @csrf
                         @method('patch')
-                        <div class="pb-4">
-                            <div class="pb-2"><label for="weekNumber"><strong class="font-semibold text-gray-900">Week Number</strong></label></div>
-                            <input type="text" placeholder="Week Number" class="input input-bordered w-full max-w-xs" value="{{ old('week_number', $learningPlan->week_number) }}" /> <br>
-                        </div>
-                        <div class="pb-4">
-                            <div class="pb-2"><label for="study_material"><strong class="font-semibold text-gray-900">Study Material</strong></label></div>
-                            <textarea class="textarea textarea-bordered w-full" placeholder="Study Material" name="Study Material"> {{ old('study_material', $learningPlans->study_material) }} </textarea>
-                        </div>
-                        <div class="pb-4">
-                            <div class="pb-2"><label for="learning_method"><strong class="font-semibold text-gray-900">Learning Method</strong></label></div>
-                            <textarea class="textarea textarea-bordered w-full" placeholder="Learning Method" name="Study Material"> {{ old('learning_method', $learningPlan->learning_method) }} </textarea>
-                        </div>
-                        <div class="pb-4">
-                            <div class="pb-2"><label for="week_number"><strong class="font-semibold text-gray-900">Estimated Time</strong></label></div>
-                            <input type="text" placeholder="Estimated Time" class="input input-bordered w-full max-w-xs" value="{{ old('estimated_time', $learningPlan->estimated_time) }}" /> <br> /> <br>
-                        </div>
+                        <label for="week_number" class="label">
+                                <span class="label-text">Kode Syllabus</span>
+                            </label>
+                            <input type="number" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="syllabus_id"/>
+
+                            <label for="week_number" class="label">
+                                <span class="label-text">Kode LLO</span>
+                            </label>
+                            <input type="number" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="llo_id"/>
+
+                            <label for="week_number" class="label">
+                                <span class="label-text">Week Number</span>
+                            </label>
+                            <input type="number" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="week_number"/>
+
+                            <label for="sudy_material" class="label">
+                                <span class="label-text">Study Material</span>
+                            </label>
+                            <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="study_material" />
+                            
+                            <label for="learning_method" class="label">
+                                <span class="label-text">Learning Method</span>
+                            </label>
+                            <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="learning_method" />
+
+                            <label for="estimated_time" class="label">
+                                <span class="label-text">Estimated Time</span>
+                            </label>
+                            <input type="text" placeholder="Masukkan input" class="input input-bordered w-full max-w-xs" name="estimated_time" />
                         <input type="submit" value="Save" class="btn btn-active rounded-md " />
-                        <a href="{{ route('syllabi.learning-plans.index', [$syllabi, $learningPlan->id]) }}" class="btn btn-outline rounded-md">Cancel</a>
+                        <a href="{{ route('syllabi.learning-plans.index', [$syllabus]) }}" class="btn btn-outline rounded-md">Cancel</a>
                 </div>
             </div>
         </div>
