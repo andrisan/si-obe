b05d7d2b802e70d518f16f692df8a5e515f2c053
Moch Danial Rasyid Akbar S <danialrasyid1@student.ub.ac.id>
2022-11-24 15:03:51 +0700

    Update create.blade.php

--- resources/views/intended-learning-outcomes/create.blade.php
+++ resources/views/intended-learning-outcomes/create.blade.php
@@ -19,12 +19,15 @@
                     <div class="flex items-left justify-left">
                         <form action="{{route('syllabi.ilos.store',[$syllabus])}}" method="post">
                             @csrf
-                            <h3 class="font-semibold text-md lg:ml-14 pt-16 tracking-widest ml-4 lg:text-lg iloPosition">POSISI</h3>
+                            <h3 class="font-semibold text-md pt-4 tracking-widest ml-4 lg:ml-14 lg:text-lg  text-black">POSISI</h3>
                             <input type="text" name ="position" id="position" placeholder="Ketik di sini..." class="input input-bordered font-regular lg:ml-14 lg:w-[30vw] text-base w-[40vw] ml-4 border-[3px] border-solid bg-white mt-4" />
-                            <h3 class="font-semibold text-md pt-4 tracking-widest ml-4 lg:ml-14 lg:text-lg iloDesc">DESKRIPSI</h3>
+                            <h3 class="font-semibold text-md pt-4 tracking-widest ml-4 lg:ml-14 lg:text-lg  text-black">DESKRIPSI</h3>
                             <input class="textarea textarea-bordered w-[70vw] ml-4 lg:ml-14 border-[3px] border-solid bg-white mt-4 h-[266px] resize-none font-regular text-base" name="description" id="description" placeholder="Ketik di sini...">
                             <br>
-                            <button type="submit"  name="submit" id="submit" class="btn btn-primary lg:text-lg w-[150px] mt-9 text-capitalize bg-[#2E65F3] border-none transition-all duration-300 ml-4 hover:bg-sky-700 lg:ml-14">submit</button>
+                            <div class="mt-5 ml-10 pl-2">
+                            <button type="submit"  name="submit" id="submit" class="mr-4 btn rounded-md hover:bg-slate-200 hover:text-black">submit</button>
+                            <a href="{{route ('syllabi.ilos.index',[$syllabus])}}" class="btn btn-outline rounded-md "> Cancel</a>
+                            </div>
                         </form>
                     </div>
                 </div>
