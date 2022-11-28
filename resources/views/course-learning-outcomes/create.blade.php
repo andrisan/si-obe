c8dc8cefb6c631a19a3107368314c5dee765a510
Irfan Hanif <14.irfanh@gmail.com>
2022-11-28 16:37:09 +0700

    added error handling for create

--- resources/views/course-learning-outcomes/create.blade.php
+++ resources/views/course-learning-outcomes/create.blade.php
@@ -17,10 +17,12 @@
                     <h2 class="font-semibold text-3xl text-center">Input Your New Course Learning Outcomes</h2>
                         <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Position</strong></label></div>
                         <input type="text" placeholder="Posisi CLO" class="input input-bordered w-full max-w-xs" name="position"/> <br>
+                        <x-input-error :messages="$errors->get('position')" class="mt-2" />
                     </div>
                     <div class="pb-4">
                     <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Description</strong></label></div>
                     <textarea class="textarea textarea-bordered w-full lg:w-3/4 h-64" placeholder="Description" name="description" spellcheck="false"></textarea>
+                    <x-input-error :messages="$errors->get('description')" class="mt-2" />
                     </div>
                     <input type="submit" value="Save" class="btn btn-active rounded-md " />
                     <a href="{{ route('syllabi.ilos.clos.index', [$syllabus, $ilo]) }}" class="btn btn-outline rounded-md">Cancel</a>
