f8e8918d61e05275dd05543fbfa24714a8408dc5
nabilghi55 <nabilghi55@student.ub.ac.id>
2022-11-28 20:42:24 +0700

    Update create.blade.php

--- resources/views/departments/create.blade.php
+++ resources/views/departments/create.blade.php
@@ -34,9 +34,26 @@
                         <label class="form-label inline-block mb-2 text-gray-700">
                             Department
                         </label>
-                        <input type="text"
-                            class="form-control block w-full px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-400 rounded"
-                            placeholder="Masukkan id fakultas" name="faculty_id">
+                       <div class="dropdown dropdown-end ml-10">
+                                <select name="forma" onchange="location = this.value;"
+                                    class="select input-bordered w-full max-w-xs" selected>
+                                    <option disabled selected>Nama Fakultas</option>
+                                    
+                                        @if (old('faculty') == $faculty->id)
+                                            <option selected value="/faculties/{{ $faculty->id }}/departments">
+                                                {{ $faculty->name }}</option>
+                                        @else
+                                            <option selected value="/faculties/{{ $faculty->id }}/departments">
+                                                {{ $faculty->name }}</option>
+                                        @endif
+
+                                </select>
+
+
+
+
+                            </div>
+
                         <input type="text"
                             class="form-control mt-5 block w-full px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-400 rounded"
                             placeholder="Nama Departement baru" name="name">
