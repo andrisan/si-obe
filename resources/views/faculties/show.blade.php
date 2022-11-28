93ccd270cbaf09c7d7e69dbf27d5bbbbf0cc65f3
supss03 <myusufalhafiz@gmail.com>
2022-11-28 20:05:41 +0700

    Update show.blade

--- resources/views/faculties/show.blade.php
+++ resources/views/faculties/show.blade.php
@@ -2,7 +2,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Daftar Fakultas') }}
+            {{ __('Daftar Departemen') }}
         </h2>
     </x-slot>
 
