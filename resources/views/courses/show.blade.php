c2e2079efaf24aab973d1bd21069d93218fb8c88
maersyifaaa <112466612+maersyifaaa@users.noreply.github.com>
2022-11-29 19:53:55 +0700

    UPDATE INDEX BY SHAFARADINE
    COMMIT BY SHAFARADINE

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -1,3 +1,5 @@
+@section('pageTitle', "Course $course->name")
+
 <x-app-layout>
     <x-slot name="header">
         <div class="flex justify-between">
@@ -25,32 +27,36 @@
                         <h2 class="card-title pb-5">{{$course->name}}</h2>
                         <div class="grid grid-cols-3 grid-flow-row gap-4">
                             <p class="py-3">Study Program</p>
-                            <div class="col-span-2">: {{$course->studyProgram()->find($course->study_program_id)->name}}</div>
+                            <div class="py-3 col-span-2">: {{$course->studyProgram()->find($course->study_program_id)->name}}</div>
                             <p class="py-3">Class Code</p>
-                            <div class="col-span-2">: {{$course->code}}</div>
+                            <div class="py-3 col-span-2">: {{$course->code}}</div>
                             <p class="py-3">Course Type</p>
-                            <div class="col-span-2">: {{$course->type}}</div>
+                            <div class="py-3 col-span-2">: {{$course->type}}</div>
                             <p class="py-3">Course Credit</p>
-                            <div class="col-span-2">: {{$course->course_credit}}</div>
+                            <div class="py-3 col-span-2">: {{$course->course_credit}}</div>
                             <p class="py-3">Lab Credit</p>
-                            <div class="col-span-2">: {{$course->lab_credit}}</div>
+                            <div class="py-3 col-span-2">: {{$course->lab_credit}}</div>
                             <p class="py-3">Learning Media</p>
-                            <div class="col-span-2">: {{$course->learning_media}}</div>
+                            <div class="py-3 col-span-2">: {{$course->learning_media}}</div>
                             <p class="py-3">Study Material</p>
-                            <div class="col-span-2">: {{$course->study_material_summary}}</div>
+                            <div class="py-3 col-span-2">: {{$course->study_material_summary}}</div>
                             <p class="py-3">Minimal Requirement</p>
-                            <div class="col-span-2">: {{$course->minimal_requirement}}</div>
+                            <div class="py-3 col-span-2">: {{$course->minimal_requirement}}</div>
                             <p class="py-3">Short Description</p>
-                            <div class="col-span-2">: {{$course->short_description}}</div>
+                            <div class="py-3 col-span-2">: {{$course->short_description}}</div>
+                            <p class="py-3">User Creator</p>
+                            <div class="py-3 col-span-2">: {{$course->creator()->find($course->creator_user_id)->name}}</div>
                         </div>
 
                         <div class="card-actions justify-end pt-20 gap-5">
-                            {{-- <form action="{{ route('courses.index') }}">--}}
-                            <button class="btn btn-outline" href="{{ route('courses.store') }}" type="submit">Edit</button>
-                            {{-- </form>--}}
-                            {{-- <form action="{{ route('courses.index') }}">--}}
-                            <a class="btn btn-outline" href="{{ route('courses.index') }}">Delete</a>
-                            {{-- </form>--}}
+                            <form action="{{ route('courses.edit',$course->id) }}" method="get">
+                                <button class="btn btn-outline" href="{{ route('courses.store') }}" type="submit">Edit</button>
+                            </form>
+                            <form action="{{route('courses.destroy', $course->id)}}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="btn btn-outline" value="{{ $course->id }}" onclick="return confirm('Yakin ingin menghapus data?');">Delete</button>
+                            </form>
                         </div>
                     </div>
                 </div>
