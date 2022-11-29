0786b290cfc7d8f8136679e1f17a31145d2d1f60
alhavydz <jundullah0853@student.ub.ac.id>
2022-11-29 13:42:27 +0700

    button

--- resources/views/faculties/index.blade.php
+++ resources/views/faculties/index.blade.php
@@ -2,7 +2,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Daftar Fakultas') }}
+            {{ __('Fakultas') }}
         </h2>
     </x-slot>
 
@@ -12,8 +12,8 @@
                 <div data-theme="light" class="p-10">
                     <div class="grid">
                         <div class="grid justify-start">
-                            <button class="btn btn-accent font-bold text-black ">
-                                <a href="{{ route('faculties.create') }}">Tambah</a>
+                            <button class="btn btn-black hover:bg-white hover:text-black text-white font-bold rounded border-black border-2 hover:border-black mb-2 btn-md">
+                                <a href="{{ route('faculties.create') }}">Tambah Fakultas</a>
                             </button>
                         </div>
                     </div>
@@ -36,7 +36,7 @@
                                         <td class="text-center">
                                             <div class="flex     justify-center">
                                                 <a href="{{ route('faculties.show', [$faculty]) }}">
-                                                    <button class="btn bg-orange-600 hover:bg-white hover:text-orange-600 text-white font-bold py-2 px-4 rounded border-orange-600 border-2 hover:border-orange-600 btn-sm m-1">Detail</button>
+                                                    <button class="btn bg-orange-600 hover:bg-white hover:text-orange-600 text-white font-bold py-2 px-4 rounded border-orange-600 border-2 hover:border-orange-600 btn-sm">Detail</button>
                                                 </a>
                                                 </form>
                                             </div>
