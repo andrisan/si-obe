462c1c05ef77dd5ebaa47b7a738704f0faf08b9f
alhavydz <jundullah0853@student.ub.ac.id>
2022-11-29 13:41:17 +0700

    button

--- resources/views/faculties/create.blade.php
+++ resources/views/faculties/create.blade.php
@@ -1,29 +1,29 @@
 <x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Tambah Fakultas') }}
-      </h2>
-  </x-slot>
-
-  <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 bg-white border-b border-gray-200">
-                  <div class="container p-4 text-primary mt-2 text-sm">
-                      <form action="{{ route('faculties.store') }}" method="post">
-                          @csrf
-                          <div class="form-control">
-                              <label class="uppercase font-bold  text-blue-600" for="fname">nama fakultas</label>
-                              <input type="text" placeholder="Type here" class="input input-bordered input-ghost input-l w-full max-w-xl mb-6" name="name"/>
-                          </div>
-                          <div class="actions justify-start">
-                              <button class="btn bg-blue-600 hover:bg-white hover:text-blue-600 text-white font-bold py-2 px-4 rounded border-blue-600 border-2 hover:border-blue-600 mr-2">Tambah</button>
-                              <button class="btn bg-red-600 hover:bg-white hover:text-red-600 text-white font-bold py-2 px-4 rounded border-red-600 border-2 hover:border-red-600"><a href="{{ route('faculties.index') }}">Cancel</a></button>
-                          </div>
-                      </form>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-</x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tambah Fakultas') }}
+        </h2>
+    </x-slot>
+  
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="container p-4 text-primary text-sm">
+                        <form action="{{ route('faculties.store') }}" method="post">
+                            @csrf
+                            <div class="form-control">
+                                <label class="uppercase font-bold  text-black mb-1" for="fname">nama fakultas</label>
+                                <input type="text" placeholder="Type here" class="input input-bordered input-ghost input-l w-full max-w-xl mb-4   " name="name"/>
+                            </div>
+                            <div class="actions justify-start">
+                                <button class="btn bg-green-500 hover:bg-white hover:text-green-500 text-white font-bold rounded border-green-500 border-2 hover:border-green-500 btn-sm mr-2">Tambah</button>
+                                <button class="btn bg-red-500 hover:bg-white hover:text-red-500 text-white font-bold rounded border-red-500 border-2 hover:border-red-500 btn-sm"><a href="{{ route('faculties.index') }}">Cancel</a></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  </x-app-layout>
