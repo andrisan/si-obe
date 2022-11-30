ecc6060be8b3c5a33dd1b26d46d8682626d482fe
fauzanakbar2503 <107809786+fauzanakbar2503@users.noreply.github.com>
2022-11-30 13:58:54 +0700

    update create.blade.php

--- resources/views/faculties/create.blade.php
+++ resources/views/faculties/create.blade.php
@@ -14,7 +14,7 @@
                             @csrf
                             <div class="form-control">
                                 <label class="uppercase font-bold  text-black mb-1" for="fname">nama fakultas</label>
-                                <input type="text" placeholder="Type here" class="input input-bordered input-ghost input-l w-full max-w-xl mb-4   " name="name"/>
+                                <input type="text" placeholder="Type here" class="input input-bordered input-ghost input-l w-full max-w-xl mb-4   " name="name" required/>
                             </div>
                             <div class="actions justify-start">
                                 <button class="btn bg-green-400 hover:bg-white hover:text-green-400 text-white font-bold rounded border-green-400 border-2 hover:border-green-400 mr-2">Tambah</button>
