ad0d0f9a71ed1fd1235ea2fcef905fdad0e7aa5b
MuhAtthar <muhatthar@gmail.com>
2022-11-30 20:21:20 +0700

    Update index.blade.php

--- resources/views/class-portofolio/index.blade.php
+++ resources/views/class-portofolio/index.blade.php
@@ -19,15 +19,9 @@
             <thead class="text-black">
                 <tr class="h-10">
                     <th rowspan="2" class="w-10 border-x-2 border-t-2 border-black">No</th>
-                    <th rowspan="2" class="w-[35rem] border-x-2 border-t-2 border-black">Sub-Capaian Mata Kuliah</th>
-                    <th colspan="10" class="w-[35rem] border-2 border-t-2 border-black">Bobot(%)</th>
-                </tr>
-                <tr class="border-x-2 border-black h-10">
-                    @foreach ($cc->assignments as $assignment)
-                        
-                    <th class="border-x-2 border-black">{{ $assignment->assignmentPlan->title }}</th>
-                     
-                    @endforeach
+                    <th rowspan="2" class="w-[50rem] border-x-2 border-t-2 border-black">Sub-Capaian Mata Kuliah</th>
+                    <th class="w-[10rem] border-2 border-t-2 border-black">Tingkat Keberhasilan</th>
+                    <th class="w-[10rem] border-2 border-t-2 border-black">Traceroute</th>
                 </tr>
             </thead>
             <tbody class="text-center text-black">
@@ -36,41 +30,17 @@
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu memahami konsep website dinamis</td>
                     <td class="border-2 border-black">10</td>
                     <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
-                    <td class="border-2 border-black">0</td>
                 </tr>
                 <tr class="border-2 border-black h-14">
                     <td class="border-2 border-black">2</td>
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu memahami perbedaan website dinamis dan statis</td>
                     <td class="border-2 border-black">0</td>
                     <td class="border-2 border-black">10</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
-                    <td class="border-2 border-black">0</td>
                 </tr>
                 <tr class="border-2 border-black h-14">
                     <td class="border-2 border-black">3</td>
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu memahami dan mengimplementasikan server-side-scripting untuk membuat website</td>
                     <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">10</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
                     <td class="border-2 border-black">0</td>
                 </tr>
                 <tr class="border-2 border-black h-14">
@@ -78,70 +48,30 @@
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu mengimplementasikan server-side-scripting untuk mengelola file di server</td>
                     <td class="border-2 border-black">0</td>
                     <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">10</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
-                    <td class="border-2 border-black">0</td>
                 </tr>
                 <tr class="border-2 border-black h-14">
                     <td class="border-2 border-black">5</td>
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu mengimplementasikan server-side-scripting untuk mengelola data di database</td>
                     <td class="border-2 border-black">0</td>
                     <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">10</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
                 </tr>
                 <tr class="border-2 border-black h-14">
                     <td class="border-2 border-black">6</td>
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu memahami konsep MVC</td>
                     <td class="border-2 border-black">0</td>
                     <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">10</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
                 </tr>
                 <tr class="border-2 border-black h-14">
                     <td class="border-2 border-black">7</td>
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu memahami dan mengimplementasikan salah satu framework PHP untuk membuat website dinamis</td>
                     <td class="border-2 border-black">0</td>
                     <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">10</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
                 </tr>
                 <tr class="border-2 border-black h-14">
                     <td class="border-2 border-black">8</td>
                     <td class="text-justify px-3 border-2 border-black">Mahasiswa mampu mengimplementasikan teknologi AJAX untuk membuat website</td>
                     <td class="border-2 border-black">0</td>
                     <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">10</td>
-                    <td class="border-2 border-black">0</td>
-                    <td class="border-2 border-black">2,5</td>
                 </tr>
             </tbody>
           </table>
@@ -150,18 +80,22 @@
         <table class="table-fixed  mx-auto">
         <thead class="text-black">
                 <tr class="h-10">
-                    <th class="w-[24rem] border-x-2 border-2 border-black">Jumlah Mahasiswa</th>
                     <th class="w-[24rem] border-x-2 border-2 border-black">Jumlah Mahasiswa Lulus</th>
+                    <th class="w-[24rem] border-x-2 border-2 border-black">Jumlah Mahasiswa Tidak Lulus</th>
                     <th class="w-[24rem] border-2 border-2 border-black">Persentase Mahasiswa Lulus(%)</th>
                     <th class="w-[24rem] border-2 border-2 border-black">Persentase Mahasiswa Tidak Lulus(%)</th>
                 </tr>
             </thead>
             <tbody class="text-center text-black">
-            <tr class="border-2 border-black h-14">
-                    <td class="border-2 border-black">55</td>
+                <tr class="border-2 border-black h-14">
                     <td class="border-2 border-black">50</td>
+                    <td class="border-2 border-black">5</td>
                     <td class="border-2 border-black">95%</td>
                     <td class="border-2 border-black">5%</td>
+                </tr>
+                <tr class="border-2 border-black h-14">
+                    <td class="border-2 border-black font-bold">Jumlah Mahasiswa</td>
+                    <td colspan="3" class="border-2 border-black">55</td>
                 </tr>
             </tbody>
         </table>
