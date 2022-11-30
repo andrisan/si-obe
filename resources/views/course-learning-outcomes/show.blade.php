a43399a23b1bdaf8149efbb91288c30467d53081
Irfan Hanif <14.irfanh@gmail.com>
2022-11-30 20:14:34 +0700

    update view for show

--- resources/views/course-learning-outcomes/show.blade.php
+++ resources/views/course-learning-outcomes/show.blade.php
@@ -1,3 +1,5 @@
+@section('pageTitle', "Detail Course Learning Outcome")
+
 <x-app-layout>
 <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -9,8 +11,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">No &emsp;&emsp;&emsp;: {{ $clo-> position }}</strong></label></div>
-                <div class="pb-2"><label for="description"><strong class="font-semibold text-gray-900 dark:text-white">Deskripsi : {{ $clo->description }}</strong></label></div>
+                <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">No &emsp;&emsp;&emsp;&nbsp;: {{ $clo-> position }}</strong></label></div>
+                <div class="pb-2"><label for="description"><strong class="font-semibold text-gray-900 dark:text-white">Deskripsi &nbsp;: {{ $clo->description }}</strong></label></div>
+                <br>
                 <table class="table-fixed w-full">
                         <thead>
                             <tr class="border-2 h-10">
