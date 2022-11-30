7d446167a2db7d9ee1f0de7a1e756f2b875d944f
alhavydz <jundullah0853@student.ub.ac.id>
2022-11-30 22:06:07 +0700

    title page section n button

--- resources/views/faculties/show.blade.php
+++ resources/views/faculties/show.blade.php
@@ -1,5 +1,5 @@
+@section('pageTitle', 'Detail Faculty')
 <x-app-layout>
-
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Detail Fakultas') }}
@@ -18,7 +18,7 @@
                             <div class="flex flex-col lg:flex-row">
                                 <div class="grid flex-grow mr-2">
                                     <a href="{{ route('faculties.index') }}">
-                                        <button class="btn rounded-full btn-black font-bold btn-sm text-white"><img class="w-6 h-6" src="{{ asset('img/icon-back.png') }}" alt=""></button>
+                                        <button class="btn rounded-full btn-black font-bold btn-sm text-white"><img class="w-5 h-5" src="{{ asset('img/icon-back.png') }}" alt=""></button>
                                     </a>
                                 </div>
                                 <div class="grid flex-grow mr-2">
@@ -39,7 +39,7 @@
                     </div>
                     <div class="grid">
                         <a href="{{ route('faculties.departments.index', [$faculty]) }}">
-                            <button class="btn btn-black hover:bg-white hover:text-black text-white font-bold rounded border-black border-2 hover:border-black btn-sm mt-8">Manage Department</button>
+                            <button class="btn bg-orange-600 hover:bg-white hover:text-orange-600 text-white font-bold py-2 px-4 rounded border-orange-600 border-2 hover:border-orange-600 btn-sm mt-8">Manage Departemen</button>
                         </a>
                     </div>
                     <div class="overflow-x-auto py-4">
@@ -48,7 +48,7 @@
                             <thead>
                                 <tr>
                                     <th>Nomor</th>
-                                    <th>Nama Department</th>
+                                    <th>Nama Departemen</th>
                                     <th>Nama Program Studi</th>
                                     <th></th>
                                 </tr>
@@ -61,7 +61,7 @@
                                         <td>{{ $department->name }}
                                             <div class="flex items-strecth">
                                                 <a href="{{ route('faculties.departments.study-programs.index', [$faculty, $department]) }}">
-                                                    <button class="btn btn-black hover:bg-white hover:text-black text-white font-bold rounded border-black border-2 hover:border-black btn-xs">Manage Prodi</button>
+                                                    <button class="btn bg-gray-400 hover:bg-white hover:text-gray-400 text-white font-bold rounded border-gray-400 border-2 hover:border-gray-400 btn-xs">Manage Prodi</button>
                                                 </a>
                                             </div>
                                         </td>
