8a7b48e1f46ac7e1c6b9121956a66491534e494f
Irfan Hanif <14.irfanh@gmail.com>
2022-11-30 17:42:04 +0700

    updated table

--- resources/views/course-learning-outcomes/show.blade.php
+++ resources/views/course-learning-outcomes/show.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">No : {{ $llo-> position }}</strong></label></div>
-                <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Deskripsi : {{ $llo->description }}</strong></label></div>
+                <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">No : {{ $clo->position }}</strong></label></div>
+                <div class="pb-2"><label for="position"><strong class="font-semibold text-gray-900 dark:text-white">Deskripsi : {{ $clo->description }}</strong></label></div>
                 <table class="table-fixed w-full">
                         <thead>
                             <tr class="border-2 h-10">
@@ -21,21 +21,8 @@
                         <tbody class=" border-2 border-black text-center">
                             @foreach ($llos as $llo)
                             <tr class="border-2 h-14">
-                            <td>{{$llo['position']}}</td>    
-                            <td class="text-justify-center">{{$llo['description']}}</td>
-                            <td class="flex space-x-8 justify-center mt-2">
-                                <button class="px-4 mt-0 text-blue-800 border-blue-800 rounded-2xl border-2">Detail</button>
-                                <a href="{{ route('syllabi.ilos.llos.edit', [$syllabus, $ilo, $llo['id']]) }}" class="mt-2"><i class="fa-solid fa-pen-to-square text-blue-800"></i></a>
-                                <form method="POST" action="{{ route('syllabi.ilos.llos.destroy', [$syllabus, $ilo, $llo['id']]) }}">
-                                    @csrf
-                                    @method('delete')
-
-                                    <button class="mt-2"
-                                            onclick="event.preventDefault(); confirm('Are you sure?') && this.closest('form').submit();">
-                                            <i class="fa-solid fa-trash-can text-red-600"></i>
-                                    </button>
-                                </form>
-                                </td>
+                            <td>{{ $llo['position'] }}</td>    
+                            <td class="text-justify-center">{{ $llo['description' ]}}</td>
                             </tr>
                             @endforeach
                         </tbody>
