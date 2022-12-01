daea1ae2cb35f8c14ff4d4d3cac3979e304acca5
Gazee <100186961+Gazee1112@users.noreply.github.com>
2022-12-01 13:38:06 +0700

    update model student grade

--- app/Models/StudentGrade.php
+++ app/Models/StudentGrade.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Assignment;
 
 class StudentGrade extends Model
 {
     use HasFactory;
+
+    public function User()
+    {
+        return $this->belongsTo(User::class, 'student_user_id');
+    }
+    public function Assignment()
+    {
+        return $this->belongsTo(Assignment::class, 'assignment_id');
+    }
+    public function Criteria_Level()
+    {
+        return $this->belongsTo(CriteriaLevel::class, 'criteria_level_id');
+    }
+    public function AssignmentPlanTask()
+    {
+        return $this->belongsTo(AssignmentPlanTask::class, 'assignment_plan_task_id');
+    }
 }
