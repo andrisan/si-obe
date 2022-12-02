0e95225ee533c2656b76476f920f3338802c835d
Irfan Hanif <14.irfanh@gmail.com>
2022-12-02 16:47:12 +0700

    added new route middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'isTeacher' => \App\Http\Middleware\MustBeTeacher::class,
     ];
 }
