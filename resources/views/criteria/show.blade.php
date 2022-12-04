a714769ac305f8e7d9a6412fb75cb41fd4feb39a
A. Fauzi <100508741+Aither-24@users.noreply.github.com>
2022-12-04 15:21:13 +0700

    rename

--- resources/views/criteria/show.blade.php
+++ resources/views/criteria/show.blade.php
@@ -15,63 +15,73 @@
                         <div class="judul  ">
                             <h1 class="text-2xl font-extrabold" style="font-weight: 900;">Criterias</h1>
                         </div>
-            
-                        
-                        
+
+
+
                     </div>
-            
+
                     <nav class="">
                         <ul class="flex space-x-2 font-extrabold">
                             <li class="text-[#2E65F3]">Rubrics <span class="mx-2">/</span> </li>
                             <li class="">Criterias <span class="mx-2"></span> </li>
                         </ul>
                     </nav>
-            
+
                     <div class="flex  mt-5 space-x-5 relative">
                         <div class="">
                             <input type="text" class="bg-[#F7F7F9] w-52 px-5 py-2 rounded-md text-black placeholder:text-black" placeholder="MASUKKAN INDIKATOR">
                         </div>
-                        
+
                         <div class="">
                             <select name="" id="" class="bg-[#F7F7F9] w-52 px-5 py-2 rounded-md text-black">
-                                
-                                    <option value="books">PILIHAN INDIKATOR</option>
-                                    <option value="html">N01-1</option>
-                                    <option value="css">N01-2</option>
-                                    <option value="php">N02-1</option>
-                                    <option value="js">N0-2</option>
-                                  
+
+                                <option value="books">PILIHAN INDIKATOR</option>
+                                <option value="html">N01-1</option>
+                                <option value="css">N01-2</option>
+                                <option value="php">N02-1</option>
+                                <option value="js">N0-2</option>
+
                             </select>
                         </div>
-                        
+
                     </div>
-            
+
                     <div class="mt-10">
                         <table class="table-fixed w-full">
                             <thead>
-                              <tr class="bg-[#F7F7F9] border-2 h-10">
-                                <th class="w-10">No</th>
-                                <th class="w-44">INDIKATOR</th>
-                                <th class="w-[39rem]">DESKRIPSI</th>
-                                <th>BOBOT(%)</th>
-                                <th>AKSI</th>
-                              </tr>
+                                <tr class="bg-[#F7F7F9] border-2 h-10">
+                                    <th class="w-16">No</th>
+                                    <th class="w-64">Title</th>
+                                    <th class="w-32">Max Point</th>
+                                    <th class="w-128">Description</th>
+                                    <th class="w-32">AKSI</th>
+                                </tr>
                             </thead>
                             <tbody class="text-center border-2 border-black text-black">
-                              <tr class="border-2 h-14">
-                                <td>1</td>
-                                <td>N01-1</td>
-                                <td>Mampu melaksanakan konsep Pemrograman Basis Data dalam pengembangan aplikasi</td>
-                                <td >2.5</td>
-                                <td><button class="px-2 border-2 border-blue-700 rounded-xl text-blue-700 text-sm font-bold">Lihat Levels</button></td>
-                              </tr>
-                            
-                              
+                                <tr class="border-2 h-14">
+                                    <td>1</td>
+                                    <td>Mampu melaksanakan konsep Pemrograman Basis Data dalam pengembangan aplikasi</td>
+                                    
+                                    <td>N01-1</td><td>2.5</td>
+                                    <td>
+                                        <div>
+                                            <button class="px-4 m-1 bg-blue-600 rounded-xl text-white text-sm font-bold">Show</button>
+                                        </div>
+                                        <div>
+                                            <button class="px-4 m-1 bg-yellow-600 rounded-xl text-white text-sm font-bold">Edit</button>
+                                           </div>
+                                           <div>
+                                            <button class="px-4 m-1 bg-red-600 rounded-xl text-white text-sm font-bold">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+
+
                             </tbody>
-                          </table>
+                        </table>
                     </div>
+                </div>
             </div>
+
         </div>
-                    
-    </div>
 </x-app-layout>
