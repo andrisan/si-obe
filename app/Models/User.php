db155dccec0adf60d838c7f407057b928c56403d
bariqalfariz <bariqalfariz09@gmail.com>
2022-12-05 15:27:17 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,8 +43,8 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
-    public function studentGrades()
+    public function studentGrade()
     {
-        return $this->hasMany(StudentGrade::class);
+        return $this->hasMany(StudentGrade::class, 'student_user_id');
     }
 }
