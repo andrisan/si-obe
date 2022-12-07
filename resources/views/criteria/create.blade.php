62c1c9beefb4e0f1eaa92c16ab617e3336b4aa7c
firdakusuma <firdakusumaaa@gmail.com>
2022-12-07 19:36:09 +0700

    Update create.blade.php

--- resources/views/criteria/create.blade.php
+++ resources/views/criteria/create.blade.php
@@ -12,47 +12,39 @@
                 <div class=" bg-white border-b border-gray-200">
                     
                 <form action="{{ route('rubrics.criterias.store', $rubric) }}" method="post">
+                @csrf
 
                     <div class="p-10">
                         <h1 class="text-black">Title</h1>
                         <div class="mt-2">
                             <input class="rounded-md w-80" placeholder="Enter text here..." type="text">
+                            <x-input-error :messages="$errors->get('title')" class="mt-2" />
                         </div>
-
-                        <div class="flex">
-
+                        
                             <div class="mt-5">
                                 <h1 class="text-black">LLO</h1>
                                 <input type="text" class="mt-2 w-80 rounded-md" placeholder="Enter Text Here..."
                                     name="" id="">
+                                    <x-input-error :messages="$errors->get('llo')" class="mt-2" />
                             </div>
 
-                            <div class="mt-5 ml-10 ">
-                                <h1 class="text-black">L E V E L</h1>
-                                <select class="w-80 mt-2 rounded-md" name="" id="">
-                                    <option disabled>Select a category..</option>
-                                    <option value="saab">Baik</option>
-                                    <option value="mercedes">Cukup</option>
-                                    <option value="audi">Kurang</option>
-                                </select>
-                            </div>
-                        </div>
 
                           <div class="mt-5">
                                 <h1 class="text-black">max point</h1>
                                 <input type="text" class="mt-2 w-80 rounded-md" placeholder="Enter Text Here..."
                                     name="" id="">
+                                    <x-input-error :messages="$errors->get('max_point')" class="mt-2" />
                             </div>
 
                         <div class="mt-5 ">
                             <h1 class="text-black">D E S C R I P T I O N</h1>
                             <textarea class="rounded-md mt-2 h-52" name="" id="" cols="30" rows="10"></textarea>
                         </div>
+                        <x-input-error :messages="$errors->get('description')" class="mt-2" />
 
                         <div class="mt-10 flex space-x-5">
-                            <button class="rounded-md hover:bg-blue-700 bg-blue-500 text-white p-3">Create</button>
-                            <button
-                                class="rounded-md border-2 border-blue-400 hover:border-blue-700 hover:text-blue-600 bg-white text-blue-500 p-3">Button</button>
+                        <input type="submit" value="CREATE" class="btn btn-active rounded-md " />
+                        <a href="{{ route('rubrics.criterias.index', [$rubric]) }}" class="btn btn-outline rounded-md">Cancel</a>
                         </div>
                     </div>
 
