8d490775015f2030302a5f2a216ec02b5331e5cd
Tanziil <100509407+Tanziil20@users.noreply.github.com>
2022-12-07 19:17:36 +0700

    Update show.blade.php

--- resources/views/criteria/show.blade.php
+++ resources/views/criteria/show.blade.php
@@ -11,60 +11,34 @@
     </x-slot>
 
     <div class="py-12 max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <div class="flex flex-row sm:justify-end mb-3 px-4 sm:px-0 -mr-2 sm:-mr-3">
-            <div class="order-5 sm:order-6 mr-2 sm:mr-3">
-                <a href="{{ route('rubrics.criterias.create', [$rubric, $criterias->id])}}" class="w-full bg-white border border-gray-300 rounded-md shadow-sm px-2.5 sm:px-4 py-2 inline-flex justify-center text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    <span class="pr-1">New Criterias</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-
-
-                </a>
-            </div>
-        </div>
-        <div class="p-20 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="flex justify-between border-b-2 pb-4 ">
-                <h1 class="text-2xl font-extrabold" style="font-weight: 900;">Criterias</h1>
-            </div>
-
-            <nav class="">
-                <ul class="flex space-x-2 font-extrabold">
-                    <li class="text-[#2E65F3]">Rubrics <span class="mx-2">/</span> </li>
-                    <li class="">Criterias <span class="mx-2"></span> </li>
-                </ul>
-            </nav>
-
-            <div class="flex  mt-5 space-x-5 relative">
-                <div class="">
-                    
+        <div class="flex flex-row sm:justify-end mb-3 px-4 sm:px-0 -mr-2 sm:-mr-3">
+             <div class="p-20 bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex justify-between border-b-2 pb-4 ">
+                    <h1 class="text-2xl font-extrabold" style="font-weight: 900;">Criterias Details</h1>
                 </div>
-                <div class="">
-                    <button></button>
-                </div>
-            </div>
 
-
-            <table class="mt-10 table-fixed w-full">
-                <thead>
-                    <tr class="bg-[#F7F7F9] border-2 h-10">
-                        <th class="w-32">No</th>
-                        <th class="w-64">Title</th>
-                        <th class="w-64">LLO </th>
-                        <th class="w-32">Max Point</th>
-                        <th class="w-128">Description</th>
-                        <th class="w-32">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody class="border-2 border-black text-black">
-                    <tr class="border-2 h-14">
-                        <td class="px-6 py-3 border-t border-gray-100">{{ $criterias->id }}</td>
-                        <td class="px-6 py-3 border-t border-gray-100">{{ $criterias->title }}</td>
-                        <td class="px-6 py-3 border-t border-gray-100">{{ $criterias->LessonLearningOutcome->description }}</td>
-                        <td class="px-6 py-3 border-t border-gray-100">{{ $criterias->max_point }}</td>
-                        <td class="px-6 py-3 border-t border-gray-100">{{ $criterias->description }}</td>
-                        <td>
-                            <div class="flex flex-wrap m-1">
+                    <tbody class="border-2 border-black text-black">
+                         <tr class="border-2 h-14">
+                            <div class="grid grid-cols-3 grid-flow-row gap-4">
+                                <p class="py-3">No</p>
+                                <div class="py-3 col-span-2">: {{$criterias->id }}</div>
+                                <p class="py-3">Title</p>
+                                <div class="py-3 col-span-2">: {{$criterias->title }}</div>
+                                <p class="py-3">LLO</p>
+                                <div class="py-3 col-span-2">: {{ $criterias->LessonLearningOutcome->description}}</div>
+                                <p class="py-3">Max Point</p>
+                                <div class="py-3 col-span-2">: {{ $criterias->max_point }}</div>
+                                <p class="py-3">Description</p>
+                                <div class="py-3 col-span-2">: {{$criterias->description}}</div>
+                                <p class="py-3">Created At</p>
+                                <div class="py-3 col-span-2">: {{$criterias->created_at}}</div>
+                                <p class="py-3">Updated At</p>
+                                <div class="py-3 col-span-2">: {{$criterias->updated_at }}</div>
+                            
+                            </div>
+                        </tr>
+                    </tbody>
+                            <div class="flex card-actions justify-center pt-5">
                                 <form action="{{ route('rubrics.criterias.edit', [$rubric, $criterias->id]) }}" method="GET">
                                     <button class="basis-full px-4 py-1 m-1 bg-yellow-600 rounded-xl text-white text-sm font-bold">Edit</button>
                                 </form>
@@ -75,10 +49,10 @@
                                     <button class="basis-full px-4 py-1 m-1 bg-red-600 rounded-xl text-white text-sm font-bold " value="{{ $criterias->id }}" onclick="return confirm('Are you sure?')">Delete</button>
                                 </form>
                             </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
 </x-app-layout>
