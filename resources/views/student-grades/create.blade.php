40e988783bcbd7c42228230326daf6e5cda110a2
alviantaa <112466435+alviantaa@users.noreply.github.com>
2022-12-08 02:24:07 +0700

    Update create.blade.php
    Create view works

--- resources/views/student-grades/create.blade.php
+++ resources/views/student-grades/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-2xl text-gray-800 leading-tight">
-            Create: Student Grades
+            Create Student Grades
         </h2>
     </x-slot>
 
@@ -10,49 +10,42 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <h1 class="text-3xl font-bold">{{ $username }}</h1>
+                    <br>
+                    <h1><b>Deskripsi tugas</b></h1>
+                    <h1>{{ $assignment }}</h1>
+                    <br>
+                    <h1 class="text-center text-2xl font-bold">Input Nilai</h1>
 
 
-                    <div class="grid grid-cols-2">
+                    <form method="post" action="{{route('student-grades.store')}}">
+                        @csrf
+                        <input type="hidden" name="assignment_id" value="{{ $assignmentid }}">
+                        <input type="hidden" name="student_user_id" value="{{ $userid }}">
+                        <br>
                         <div>
-                            <h2 class="text-lg"><b>{{ $grades->first()->user->name}}</b></h2>
-                            <p>Status Pengumpulan</p>
-                        </div>
-                        <div class="text-right">
-                            <h2 class="text-lg"><b>100/100</b></h2>
-                            <p>Status Penilaian</p>
+                            <h1 class="text-xl font-bold">Masukkan Learning Plan Task</h1>
+                            <select name="assignment_plan_task_id" placeholder="-">
+                                @foreach ($plantasks as $plan)
+                                <option value="{{$plan->id}}">{{$plan->code}} | {{Str::limit($plan->description, 130)}}</option>
+                                @endforeach
+                            </select>
                         </div>
                         <br>
-
-                    </div>
-
-                    <p>Mampu menjelaskan konsep pemrograman basis data dalam pengembangan aplikasi</p>
-                    <br>
-
-                    <form method="post" action="{{route('student-grades.store', $grades->first()->id) }}">
-                        <input type="hidden" name="assignment_id" value="{{ $grades->first()->assignment_id }}">
-                        <input type="hidden" name="user_id" value="{{ $grades->first()->student_user_id }}">
-                        @csrf
-                        @method('patch')
-                        @foreach($grades as $grade)
-                            <div class="btn-group">
-                                @foreach($grade->range as $criteria)
-                                    <input type="radio" name="criteria_level_id{{ $loop->parent->index }}" data-title="{{ $criteria->point }}" class="btn px-16" value="{{ $criteria->id }}" @checked($criteria->id === $grade->criteria_level_id)/>
+                        <div>
+                            <h1 class="text-xl font-bold">Masukkan Criteria Level</h1>
+                            <select name="criteria_level_id" placeholder="-">
+                                @foreach ($criterialevels as $criterialevel)
+                                <option value="{{$criterialevel->id}}">{{$criterialevel->point}} | {{Str::limit($criterialevel->title)}} | {{Str::limit($criterialevel->description,105)}}</option>
                                 @endforeach
-                            </div>
-                            <br>
-                        @endforeach
-
-                        <div class="card-actions justify-end pt-5">
-                            {{-- <form action="{{ route('student-grades.index') }}">--}}
-                            <button class="btn btn-outline" type="submit">Save</button>
-
-                            {{-- <form action="{{ route('student-grades.index') }}">--}}
-                            <a class="btn btn-outline" href="/student-grades/?assignment_id={{ $grades->first()->assignment_id }}">Cancel</a>
-
+                            </select>
                         </div>
-
+                        <div>
+                            <input type="submit" value="Input nilai" class="btn btn-success m-2">
+                            <a href="{{route('student-grades.store')}}"></a>
+                        </div>
+                        
                     </form>
-
 
 
                 </div>
