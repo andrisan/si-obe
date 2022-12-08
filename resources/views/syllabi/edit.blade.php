a32575b92ad130798c42197e8c92283de7895f26
Rizka Winal Sabrina <103319544+rizkawinalsabrina@users.noreply.github.com>
2022-12-08 12:28:10 +0700

    Update edit.blade.php

--- resources/views/syllabi/edit.blade.php
+++ resources/views/syllabi/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Syllabi Edit of '.$syllabus) }}
+      {{ __('Syllabi Edit of ' . "$syllabus->title") }}
     </h2>
   </x-slot>
 
@@ -15,14 +15,17 @@
 
   <div class="px-8 pt-3 pb-10">
     <div class="border-b rounded-lg bg-primary-content shadow-xl px-5 py-5">
-
+          <form action="{{route('syllabi.update',$syllabus)}}" method="POST">
+              @csrf
+              @method('patch')
       <div class="grid grid-cols-1 pb-5">
         <div>
           <div class="form-control w-full px-10">
             <label class="label">
               <span class="label-text text-neutral font-bold">Syllabus Name</span>
             </label>
-            <input type="text" class="input text-neutral input-bordered bg-white w-full h-18" />
+            <input type="text" class="input text-neutral input-bordered bg-white w-full h-18"
+            value="{{ $syllabus->title }}" name="title" />
           </div>
           <div class="form-control w-full px-10">
             <label class="label">
@@ -45,6 +48,7 @@
           </div>
         </div>
       </div>
+   </form>
 
     </div>
   </div>
