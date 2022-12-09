84e087622623a4146904734b3568104679eb16a7
kevnlj <103430507+kevnlj@users.noreply.github.com>
2022-12-09 19:22:35 +0700

    Update grade.blade.php

--- resources/views/profile/grade.blade.php
+++ resources/views/profile/grade.blade.php
@@ -11,9 +11,6 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="col-span-3 p-10 shadow-xl justify-items-auto overflow-y-auto">
                         <!--"text-4xl font-semibold text-primary-content">Student Grade - Assignment 1</h1>-->
-                        <div class="grid grid-cols-8 gap-4 py-4">
-                            <button class="btn btn-accent col-end-1">Cetak</button>
-                        </div>
                         <div class="overflow-x-auto py-4">
                             <table class="table table-zebra w-full">
                                 <!-- head -->
@@ -24,8 +21,9 @@
                                         <th>NAMA MATA KULIAH</th>
                                         <th>SKS</th>
                                         <th>NILAI</th>
-                                        <th>KET</th>
-                                        <th>Detail Nilai</th>
+                                        <th>NILAI HURUF</th>
+                                        <th>Detail Nilai Lembar Kerja</th>
+                                        <th>Detail Nilai Sub CPMK</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -38,7 +36,7 @@
                                         <td>{{ $cc->course->name }}</td>
                                         <td>{{ $cc->course->course_credit }}</td>
                                         <td>{{ $grade }}</td>
-                                        <td></td>
+                                        <td>{{ $gradeLetter }}</td>
                                         <td>
                                             <!--<button class="btn btn-success btn-sm">Tampilkan</button>-->
                                             <!-- The button to open modal -->
@@ -47,16 +45,19 @@
                                             <!-- Put this part before </body> tag -->
                                             <input type="checkbox" id="my-modal-3" class="modal-toggle" />
                                             <div class="modal">
-                                                <div class="modal-box relative">
-                                                    <label for="my-modal-3"
-                                                        class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                                                    <h3 class="text-lg font-bold">Detail Nilai Mata Kuliah</h3>
-                                                    <div class="table">
-                                                        <table class="table table-zebra w-full">
+                                                <div class="modal-overlay relative mx-auto">
+                                                    <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                                    <h3 class="text-lg font-bold">Detail Nilai Lembar Kerja</h3>
+                                                    <div class="table modal-box mx-auto">
+                                                        <table class="table table-zebra">
                                                             <thead>
                                                                 <tr>
+                                                                    <th></th>
+                                                                    <th class="text-lg font-bold" colspan="3">Detail Nilai Lembar Kerja</th>
+                                                                </tr>
+                                                                <tr>
                                                                     <th>No</th>
-                                                                    <th>Assignment</th>
+                                                                    <th>Lembar Kerja</th>
                                                                     <th>NILAI</th>
                                                                 </tr>
                                                             </thead>
@@ -66,16 +67,17 @@
                                                                 <tr>
                                                                     <td>{{ $j }}</td>
                                                                     <td>{{ $ass->assignmentPlan->title }}</td>
-                                                                    <?php $temp = 0; ?>
+                                                                    <?php $point = 0; $maxPoint = 0;?>
                                                                     @foreach ($ass->studentGrades as $stud)
                                                                     <?php 
                                                                         if ($stud->student_user_id != $user->id) {
                                                                             continue;
                                                                         }
-                                                                        $temp += $stud->criteriaLevel->point; 
+                                                                        $point += $stud->criteriaLevel->point; 
+                                                                        $maxPoint += $stud->criteriaLevel->criteria->max_point;
                                                                         ?>
                                                                     @endforeach
-                                                                    <td>{{ $temp }}</td>
+                                                                    <td>{{ round($point / $maxPoint * 100, 2) }} %</td>
                                                                 </tr>
                                                                 <?php $j++; ?>
                                                                 @endforeach
@@ -85,6 +87,62 @@
                                                 </div>
                                             </div>
                                         </td>
+
+                                        <td>
+                                            <!--<button class="btn btn-success btn-sm">Tampilkan</button>-->
+                                            <!-- The button to open modal -->
+                                            <label for="my-modal-2" class="btn">Tampilkan</label>
+
+                                            <!-- Put this part before </body> tag -->
+                                            <input type="checkbox" id="my-modal-2" class="modal-toggle" />
+                                            <div class="modal">
+                                                <div class="modal-overlay relative mx-auto">
+                                                    <label for="my-modal-2" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                                    <h3 class="text-lg font-bold">Detail Nilai Sub CPMK</h3>
+                                                    <div class="table modal-box mx-auto">
+                                                        <table class="table table-zebra">
+                                                            <thead>
+                                                                <tr>
+                                                                    <th></th>
+                                                                    <th class="text-lg font-bold" colspan="3">Detail Nilai Sub CPMK</th>
+                                                                </tr>
+                                                                <tr>
+                                                                    <th>No</th>
+                                                                    <th>Sub CPMK</th>
+                                                                    <th>NILAI</th>
+                                                                </tr>
+                                                            </thead>
+                                                            <tbody>
+                                                                <?php $j = 1; ?>
+                                                                @foreach ($llo as $llo)
+                                                                <tr>
+                                                                    <td>{{ $j }}</td>
+                                                                    <td>{{ $llo->description }}</td>
+                                                                    <?php $point = 0; $maxPoint = 0; ?>
+                                                                    @foreach ($llo->criteria as $crit)
+                                                                    <?php 
+                                                                        $maxPoint += $crit->max_point; 
+                                                                    ?>
+                                                                    @foreach ($user->studentGrade as $stud)
+                                                                    <?php
+                                                                        if ($stud->criteriaLevel->criteria_id != $crit->id) {
+                                                                            continue;
+                                                                        }
+                                                                        $point += $stud->criteriaLevel->point;
+                                                                    ?>
+                                                                    @endforeach
+                                                                    @endforeach
+                                                                    <td>{{ round($point / $maxPoint * 100, 2) }} %</td>
+                                                                </tr>
+                                                                <?php $j++; ?>
+                                                                @endforeach
+                                                            </tbody>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+
                                     </tr>
                                     <?php $i++; ?>
                                     @endforeach
@@ -93,6 +151,7 @@
                                         <td></td>
                                         <td>Jumlah SKS : </td>
                                         <td>{{ $totalCredit }}</td>
+                                        <td></td>
                                         <td></td>
                                         <td></td>
                                         <td>
