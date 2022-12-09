b325732e2b4dfbf22f4f21ff289ae8455d876272
ilhamarkan2004 <ilhamarkan2004@gmail.com>
2022-12-09 11:53:18 +0700

    Update index.blade.php

--- resources/views/course-classes/index.blade.php
+++ resources/views/course-classes/index.blade.php
@@ -51,7 +51,7 @@
                             <img class="w-full" src="{{ asset('img/GambarCourse 1.png')}}">
                             <div class="card-body p-6">
                                 <div class="card-title text-neutral font-extrabold">
-                                    <a class="mb-1 relative group text-blue-400" href="{{route('classes.show',[$class['course_id']])}}">
+                                    <a class="mb-1 relative group text-blue-400" href="{{route('classes.show',[$class['id']])}}">
                                         <span>{{$class->course->name}}</span>
                                         <span class="absolute -bottom-1 left-1/2 w-0 h-0.5 bg-blue-400 group-hover:w-1/2 group-hover:transition-all"></span>
                                         <span class="absolute -bottom-1 right-1/2 w-0 h-0.5 bg-blue-400 group-hover:w-1/2 group-hover:transition-all"></span>
