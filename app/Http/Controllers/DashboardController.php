750448cb3fe93dd380bdcf3c3331d81057dff576
Andri Santoso <andrisanx@gmail.com>
2022-12-12 09:10:57 +0700

    init home page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,6 +23,8 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', [
+            'user' => auth()->user(),
+        ]);
     }
 }
