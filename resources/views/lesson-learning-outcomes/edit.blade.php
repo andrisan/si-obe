c996ee40092efdb5017ed24894bd29b492f5a85d
Andri Santoso <andrisanx@gmail.com>
2022-12-18 10:20:08 +0700

    Update edit.blade.php

--- resources/views/lesson-learning-outcomes/edit.blade.php
+++ resources/views/lesson-learning-outcomes/edit.blade.php
@@ -3,7 +3,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit Lesson Learning Outcome (LLO)') }}
+            {{ __('Edit Lesson Learning Outcome') }}
         </h2>
     </x-slot>
 
