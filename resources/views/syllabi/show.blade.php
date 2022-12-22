b14894d268b44c9f41f8356fa49a3792ff252b39
Andri Santoso <andrisanx@gmail.com>
2022-12-22 08:47:03 +0700

    Update show.blade.php

--- resources/views/syllabi/show.blade.php
+++ resources/views/syllabi/show.blade.php
@@ -326,7 +326,7 @@
                         foreach ($assignmentPlans as $assignmentPlan) {
                             $assignmentPlanTasks = $assignmentPlan->assignmentPlanTasks;
                             foreach ($assignmentPlanTasks as $assignmentPlanTask) {
-                                $totalMaxPoint += $assignmentPlanTask->criteria->max_point;
+                                $totalMaxPoint += $assignmentPlanTask->criteria->max_point ?? 0;
                             }
                         }
                     ?>
@@ -477,13 +477,11 @@
 
                                     <div class="flex flex-row justify-between py-6">
                                         <p class="font-semibold">{{ __("Assignment Plan Tasks") }}</p>
-                                        @unless(empty($assignmentPlan->rubric))
                                         <div class="order-5">
                                             <x-button-link href="{{ route('syllabi.assignment-plans.assignment-plan-tasks.create', [$syllabus, $assignmentPlan]) }}">
                                                 <i class="fa fa-plus"></i> {{ __('Create New Task') }}
                                             </x-button-link>
                                         </div>
-                                        @endunless
                                     </div>
                                     @if($assignmentPlanTasks->count() > 0)
                                         <div class="mb-5 overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative">
@@ -542,16 +540,9 @@
                                             </table>
                                         </div>
                                     @else
-                                        @if(empty($assignmentPlan->rubric))
-                                            <div class="flex flex-col items-center justify-center">
-                                                <p class="text-center text-gray-500">{{ __("No assignment plan tasks yet.") }}</p>
-                                                <p class="text-center text-gray-500">{{ __("Please create a rubric first.") }}</p>
-                                            </div>
-                                        @else
-                                        <div class="text-center p-8">
-                                            <p class="text-gray-600">No tasks available</p>
+                                        <div class="p-8">
+                                            <p class="text-center text-gray-500">{{ __("No assignment plan tasks yet.") }}</p>
                                         </div>
-                                        @endif
                                     @endif
                                 </div>
                             </div>
